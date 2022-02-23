824ef228a7ab9902eea0cfd3b9e1fa6fc8d2b567
Bellocci <francesco.bellocci@stud.unifi.it>
2022-02-23 16:02:17 +0100

    Created view that return all sport and championship

--- django/api/views.py
+++ django/api/views.py
@@ -1,3 +1,30 @@
+import json
+
 from django.shortcuts import render
 
-# Create your views here.
+from django.http.response import JsonResponse
+
+from api.models import Championship, Sport
+from api.serializers import ChampionshipSerializer, SportSerializer
+
+# Return all sport from the database
+def __getSportList(request):
+    sports = Sport.objects.all()
+    sports_serializer = SportSerializer(sports, many = True)
+    return sports_serializer
+
+# Return all championship from the database
+def __getChampionshipList(request):
+    championship = Championship.objects.all()
+    championship_serializer = ChampionshipSerializer(championship, many = True)
+    return championship_serializer
+
+# Return a dictionary with all sport and championship
+def getSportCategories(request):
+    sports_serializer = __getSportList(request)
+    championship_serializer = __getChampionshipList(request)
+
+    serializer_list = {}
+    serializer_list = {'sport': sports_serializer.data, 'championship': championship_serializer.data}
+
+    return JsonResponse(serializer_list, safe = False)
