a25eeba2f4fe75c85ebcaf75cab050f8bd2e5258
Bellocci <francesco.bellocci@stud.unifi.it>
2022-02-23 16:49:46 +0100

    Generated component sport-categories

---
+++ angular/myfanta-app/src/app/sport-categories/sport-categories.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sport-categories',
+  templateUrl: './sport-categories.component.html',
+  styleUrls: ['./sport-categories.component.css']
+})
+export class SportCategoriesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
