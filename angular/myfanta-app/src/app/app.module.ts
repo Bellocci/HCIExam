20d84d45d4903fe4a14d9c0464a6fe3ec2de636d
Bellocci <francesco.bellocci@stud.unifi.it>
2022-02-24 17:55:46 +0100

    Imported material-modules.ts file

--- angular/myfanta-app/src/app/app.module.ts
+++ angular/myfanta-app/src/app/app.module.ts
@@ -5,15 +5,17 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SportCategoriesComponent } from './sport-categories/sport-categories.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './material-module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SportCategoriesComponent
+    SportCategoriesComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    MaterialModule,
     BrowserAnimationsModule
   ],
   providers: [],
