b4d71c1a46b03d92e44553e872c606dc4340a91b
Bellocci <francesco.bellocci@stud.unifi.it>
2022-03-01 11:45:39 +0100

    Deleted getSportCategories views. Defined getSportsList and getChampionsipsList views.

--- django/api/urls.py
+++ django/api/urls.py
@@ -2,5 +2,6 @@ from django.urls import re_path
 from api import views
 
 urlpatterns = [
-    re_path(r'^$', views.getSportCategories)
+    re_path(r'^sports/$', views.getSportsList),
+    re_path(r'^championships/$', views.getChampionshipsList),
 ]
--- django/api/views.py
+++ django/api/views.py
@@ -8,23 +8,13 @@ from api.models import Championship, Sport
 from api.serializers import ChampionshipSerializer, SportSerializer
 
 # Return all sport from the database
-def __getSportList(request):
+def getSportsList(request):
     sports = Sport.objects.all()
     sports_serializer = SportSerializer(sports, many = True)
-    return sports_serializer
+    return JsonResponse(sports_serializer.data, safe = False)
 
 # Return all championship from the database
-def __getChampionshipList(request):
+def getChampionshipsList(request):
     championship = Championship.objects.all()
     championship_serializer = ChampionshipSerializer(championship, many = True)
-    return championship_serializer
-
-# Return a dictionary with all sport and championship
-def getSportCategories(request):
-    sports_serializer = __getSportList(request)
-    championship_serializer = __getChampionshipList(request)
-
-    serializer_list = {}
-    serializer_list = {'sport': sports_serializer.data, 'championship': championship_serializer.data}
-
-    return JsonResponse(serializer_list, safe = False)
+    return JsonResponse(championship_serializer.data, safe = False)
