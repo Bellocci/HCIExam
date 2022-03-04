da9178237357b181f73ad5b22357740e56139783
Bellocci <francesco.bellocci@stud.unifi.it>
2022-03-04 18:03:04 +0100

    Defined mat-grid-list which columns and rows that change value with size of windows.

--- angular/myfanta-app/src/app/create-team/create-team.component.ts
+++ angular/myfanta-app/src/app/create-team/create-team.component.ts
@@ -7,9 +7,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreateTeamComponent implements OnInit {
 
+  breakpoint:number = 0;
+
+  cols_tabs: number = 0;
+  rows_tabs: number = 0;
+
+  cols_buttons: number = 0;
+  rows_buttons: number = 0;
+
   constructor() { }
 
   ngOnInit(): void {
+    this.breakpoint = window.innerWidth >= 480 ? 4 : 1;
+
+    this.cols_tabs = window.innerWidth >= 480 ? 3 : 1;
+    this.rows_tabs = window.innerWidth >= 480 ? 6 : 4;
+  
+    this.cols_buttons = window.innerWidth >= 480 ? 1 : 1;
+    this.rows_buttons = window.innerWidth >= 480 ? 6 : 4;
+  }
+
+  onResize(event:any) {
+    this.breakpoint = (event.target.innerWidth >= 480) ? 4 : 1;
+
+    this.cols_tabs = (event.target.innerWidth >= 480) ? 3 : 1;
+    this.rows_tabs = (event.target.innerWidth >= 480) ? 6 : 4;
+  
+    this.cols_buttons = (event.target.innerWidth >= 480) ? 1 : 1;
+    this.rows_buttons = (event.target.innerWidth >= 480) ? 6 : 4;
   }
 
 }
