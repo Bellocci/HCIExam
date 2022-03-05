73bf62964afb9a1602d6f014ee950977c434afef
Bellocci <francesco.bellocci@stud.unifi.it>
2022-03-05 16:06:04 +0100

    Changed rows and columns when window's width is more grater than 480

--- angular/myfanta-app/src/app/create-team/create-team.component.ts
+++ angular/myfanta-app/src/app/create-team/create-team.component.ts
@@ -18,23 +18,23 @@ export class CreateTeamComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
-    this.breakpoint = window.innerWidth >= 480 ? 4 : 1;
+    this.breakpoint = window.innerWidth >= 480 ? 5 : 1;
 
     this.cols_tabs = window.innerWidth >= 480 ? 3 : 1;
-    this.rows_tabs = window.innerWidth >= 480 ? 6 : 4;
+    this.rows_tabs = window.innerWidth >= 480 ? 2 : 1;
   
-    this.cols_buttons = window.innerWidth >= 480 ? 1 : 1;
-    this.rows_buttons = window.innerWidth >= 480 ? 6 : 4;
+    this.cols_buttons = window.innerWidth >= 480 ? 2 : 1;
+    this.rows_buttons = window.innerWidth >= 480 ? 2 : 1;
   }
 
   onResize(event:any) {
-    this.breakpoint = (event.target.innerWidth >= 480) ? 4 : 1;
+    this.breakpoint = (event.target.innerWidth >= 480) ? 5 : 1;
 
     this.cols_tabs = (event.target.innerWidth >= 480) ? 3 : 1;
-    this.rows_tabs = (event.target.innerWidth >= 480) ? 6 : 4;
+    this.rows_tabs = (event.target.innerWidth >= 480) ? 2 : 1;
   
-    this.cols_buttons = (event.target.innerWidth >= 480) ? 1 : 1;
-    this.rows_buttons = (event.target.innerWidth >= 480) ? 6 : 4;
+    this.cols_buttons = (event.target.innerWidth >= 480) ? 2 : 1;
+    this.rows_buttons = (event.target.innerWidth >= 480) ? 2 : 1;
   }
 
 }
