18fbf85cbc13434723ccc1f16f664a272d40c69b
Bellocci <francesco.bellocci@stud.unifi.it>
2022-03-09 16:16:51 +0100

    Added method and variable to set the table width.

--- angular/myfanta-app/src/app/internal.service.ts
+++ angular/myfanta-app/src/app/internal.service.ts
@@ -7,11 +7,18 @@ import { BehaviorSubject } from 'rxjs';
 export class InternalService {
 
   private table_height = new BehaviorSubject('0px');
+  private table_width = new BehaviorSubject('0px');
+
   current_table_height = this.table_height.asObservable();
+  current_table_width = this.table_width.asObservable();
 
   constructor() { }
 
   setTableHeight(height:number) {
     this.table_height.next(height.toString());
+  }
+
+  setTableWidth(width:number) {
+    this.table_width.next(width.toString());
   }
 }
