ff7b23ce3fb2e473bbb8d59ce6c4bc059bde5431
Bellocci <francesco.bellocci@stud.unifi.it>
2022-03-11 10:57:46 +0100

    Added variable and methods to set the width and height options component

--- angular/myfanta-app/src/app/internal.service.ts
+++ angular/myfanta-app/src/app/internal.service.ts
@@ -8,9 +8,13 @@ export class InternalService {
 
   private table_height = new BehaviorSubject('0px');
   private table_width = new BehaviorSubject('0px');
-
   current_table_height = this.table_height.asObservable();
   current_table_width = this.table_width.asObservable();
+
+  private options_height = new BehaviorSubject('0px');
+  private options_width = new BehaviorSubject('0px');
+  current_options_height = this.options_height.asObservable();
+  current_options_width = this.options_width.asObservable();
 
   constructor() { }
 
@@ -20,5 +24,13 @@ export class InternalService {
 
   setTableWidth(width:number) {
     this.table_width.next(width.toString());
+  }
+
+  setOptionsHeight(height:number) {
+    this.options_height.next(height.toString());
+  }
+
+  setOptionsWidth(width:number) {
+    this.options_width.next(width.toString());
   }
 }
