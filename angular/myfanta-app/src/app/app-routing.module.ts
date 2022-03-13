4378ae6dec78be98a8538a8047874597df93cda5
Bellocci <francesco.bellocci@stud.unifi.it>
2022-03-13 16:10:57 +0100

    Associated path 'home' with HomeComponent

--- angular/myfanta-app/src/app/app-routing.module.ts
+++ angular/myfanta-app/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CreateTeamComponent } from './create-team/create-team.component';
+import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
-  {path: 'createTeam', component: CreateTeamComponent}
+  {path: 'createTeam', component: CreateTeamComponent},
+  {path: 'home', component: HomeComponent}
 ];
 
 @NgModule({
