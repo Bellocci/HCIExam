59e63e0bfcb2e89f77a561f933dcbda8e7ae7d4a
Bellocci <francesco.bellocci@stud.unifi.it>
2022-03-13 11:36:20 +0100

    Created sport and championship model. Created a const data for sport and championsip

---
+++ angular/myfanta-app/src/model/championship.model.ts
@@ -0,0 +1,28 @@
+export class Championship {
+    championshipId:number = -1;
+    championshipName:string = '';
+    sport:number = -1;
+}
+
+export const CHAMPIONSHIP_DATA:Championship[] = [
+    {
+        championshipId : 1,
+        championshipName : 'Serie A',
+        sport : 1
+    },
+    {
+        championshipId : 2,
+        championshipName : 'Premier League',
+        sport : 1
+    },
+    {
+        championshipId : 3,
+        championshipName : 'Serie A Female',
+        sport : 2
+    },
+    {
+        championshipId : 4,
+        championshipName : 'NBA',
+        sport : 3
+    },
+]
---
+++ angular/myfanta-app/src/model/sport.model.ts
@@ -0,0 +1,19 @@
+export class Sport {
+    sportId:number = -1;
+    sportName: string = '';
+}
+
+export const SPORT_DATA:Sport[] = [
+    {
+      sportId : 1,
+      sportName : 'Football soccer'
+    },
+    {
+      sportId : 2,
+      sportName : 'Volleyball'
+    },
+    {
+      sportId : 3,
+      sportName : 'Basketball'
+    }
+]
