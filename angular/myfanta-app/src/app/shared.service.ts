9e414922fe0318dc31c70a82b95e1a54c68a068b
Bellocci <francesco.bellocci@stud.unifi.it>
2022-03-13 11:53:37 +0100

    Modified method getSportList and getChampionshipList which return SPORT_DATA and CHAMPIONSHIP_DATA respectively.

--- angular/myfanta-app/src/app/shared.service.ts
+++ angular/myfanta-app/src/app/shared.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { SPORT_DATA } from 'src/model/sport.model';
+import { CHAMPIONSHIP_DATA } from 'src/model/championship.model';
 
 @Injectable({
   providedIn: 'root'
@@ -11,10 +13,10 @@ export class SharedService {
   constructor(private http:HttpClient) { }
 
   getSportList():Observable<any[]> {
-    return this.http.get<any[]>(this.APIUrl + '/sports/');
+    return SPORT_DATA;
   }
 
   getChampionshipList():Observable<any[]> {
-    return this.http.get<any[]>(this.APIUrl + '/championships/');
+    return CHAMPIONSHIP_DATA;
   }
 }
