a18d52ac7c1d82bc67582d1c53e8857b8d31fc9f
Bellocci <francesco.bellocci@stud.unifi.it>
2022-03-13 20:31:11 +0100

    Defined variable championship_selected and method setChampionshipSelected.

--- angular/myfanta-app/src/app/internal-data.service.ts
+++ angular/myfanta-app/src/app/internal-data.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -6,4 +7,11 @@ import { Injectable } from '@angular/core';
 export class InternalDataService {
 
   constructor() { }
+
+  private championship_selected = new BehaviorSubject('');
+  current_championship = this.championship_selected.asObservable();
+
+  setChampionshipSelected(champ:string) {
+    this.championship_selected.next(champ);
+  }
 }
