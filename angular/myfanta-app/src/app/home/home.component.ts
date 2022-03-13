6b500ef0052136de3210bcff16c08d1c464a35c1
Bellocci <francesco.bellocci@stud.unifi.it>
2022-03-13 15:54:21 +0100

    Added method to load sport and championship list. They used service to get them. Added method to check if a championship belongs to a sport

--- angular/myfanta-app/src/app/home/home.component.ts
+++ angular/myfanta-app/src/app/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Output } from '@angular/core';
 import { EventEmitter } from '@angular/core';
+import { SharedService } from '../shared.service';
 
 @Component({
   selector: 'app-home',
@@ -8,13 +9,37 @@ import { EventEmitter } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
   panelOpenState:boolean = false;
+
+  sportsList:any = [];
+  championshipsList:any = [];
+
+  constructor(private service:SharedService) { }
 
   // Share the value with parent
   @Output() childToParent = new EventEmitter<String>();
 
   ngOnInit(): void {
+    this.loadSportsList();
+    this.loadChampionshipsList();
+  }
+
+  loadSportsList() {
+    this.service.getSportList().subscribe(data => {
+      this.sportsList = data;
+    });
+  }
+
+  loadChampionshipsList() {
+    this.service.getChampionshipList().subscribe(data => {
+      this.championshipsList = data;
+    });
+  }
+
+  filterChampionship(champ:any, sport:any):boolean {
+    if(champ.sport != sport.sportId)
+      return false;
+    return true;
   }
 
   sendChampionshipSelected(name:string) {
