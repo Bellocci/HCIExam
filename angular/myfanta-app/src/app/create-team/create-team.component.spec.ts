96e84b3cae622a46c72a29caaaa2f55f79f3f122
Bellocci <francesco.bellocci@stud.unifi.it>
2022-03-14 19:10:37 +0100

    Defined unit test for create-team.component.ts

--- angular/myfanta-app/src/app/create-team/create-team.component.spec.ts
+++ angular/myfanta-app/src/app/create-team/create-team.component.spec.ts
@@ -1,4 +1,5 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, fakeAsync, TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 import { CreateTeamComponent } from './create-team.component';
 
@@ -8,7 +9,12 @@ describe('CreateTeamComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ CreateTeamComponent ]
+      imports: [
+        HttpClientTestingModule,
+      ],
+      declarations: [ 
+        CreateTeamComponent 
+      ]
     })
     .compileComponents();
   });
@@ -22,4 +28,297 @@ describe('CreateTeamComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  describe('ngOnInit', () => {
+
+    it('breakpoint value should be 5 when getInnerWidth return a value greater than 480', fakeAsync(() => {
+      let width = 1000;
+      let spy_window = spyOn(component, 'getInnerWidth').and.returnValue(width);
+
+      component.ngOnInit();
+
+      expect(component.breakpoint).toEqual(5);
+    }));
+
+    it('breakpoint value should be 5 when getInnerWidth return a value equal to 480', fakeAsync(() => {
+      let width = 480;
+      let spy_window = spyOn(component, 'getInnerWidth').and.returnValue(width);
+
+      component.ngOnInit();
+
+      expect(component.breakpoint).toEqual(5);
+    }));
+
+    it('breakpoint value should be 1 when getInnerWidth return a value less than 480', fakeAsync(() => {
+      let width = 300;
+      let spy_window = spyOn(component, 'getInnerWidth').and.returnValue(width);
+
+      component.ngOnInit();
+
+      expect(component.breakpoint).toEqual(1);
+    }));
+
+    it('cols_tabs value should be 3 when getInnerWidth return a value greater than 480', fakeAsync(() => {
+      let width = 1000;
+      let spy_window = spyOn(component, 'getInnerWidth').and.returnValue(width);
+
+      component.ngOnInit();
+
+      expect(component.cols_tabs).toEqual(3);
+    }));
+
+    it('cols_tabs value should be 3 when getInnerWidth return a value equal to 480', fakeAsync(() => {
+      let width = 480;
+      let spy_window = spyOn(component, 'getInnerWidth').and.returnValue(width);
+
+      component.ngOnInit();
+
+      expect(component.cols_tabs).toEqual(3);
+    }));
+
+    it('cols_tab value should be 1 when getInnerWidth return a value less than 480', fakeAsync(() => {
+      let width = 300;
+      let spy_window = spyOn(component, 'getInnerWidth').and.returnValue(width);
+
+      component.ngOnInit();
+
+      expect(component.cols_tabs).toEqual(1);
+    }));
+
+    it('rows_tabs value should be 6 when getInnerWidth return a value greater than 480', fakeAsync(() => {
+      let width = 1000;
+      let spy_window = spyOn(component, 'getInnerWidth').and.returnValue(width);
+
+      component.ngOnInit();
+
+      expect(component.rows_tabs).toEqual(6);
+    }));
+
+    it('rows_tabs value should be 6 when getInnerWidth return a value equal to 480', fakeAsync(() => {
+      let width = 480;
+      let spy_window = spyOn(component, 'getInnerWidth').and.returnValue(width);
+
+      component.ngOnInit();
+
+      expect(component.rows_tabs).toEqual(6);
+    }));
+
+    it('rows_tabs value should be 1 when getInnerWidth return a value less than 480', fakeAsync(() => {
+      let width = 300;
+      let spy_window = spyOn(component, 'getInnerWidth').and.returnValue(width);
+
+      component.ngOnInit();
+
+      expect(component.rows_tabs).toEqual(1);
+    }));
+
+    it('cols_buttons value should be 2 when getInnerWidth return a value greater than 480', fakeAsync(() => {
+      let width = 1000;
+      let spy_window = spyOn(component, 'getInnerWidth').and.returnValue(width);
+
+      component.ngOnInit();
+
+      expect(component.cols_buttons).toEqual(2);
+    }));
+
+    it('cols_buttons value should be 2 when getInnerWidth return a value equal to 480', fakeAsync(() => {
+      let width = 480;
+      let spy_window = spyOn(component, 'getInnerWidth').and.returnValue(width);
+
+      component.ngOnInit();
+
+      expect(component.cols_buttons).toEqual(2);
+    }));
+
+    it('cols_buttons value should be 1 when getInnerWidth return a value less than 480', fakeAsync(() => {
+      let width = 300;
+      let spy_window = spyOn(component, 'getInnerWidth').and.returnValue(width);
+
+      component.ngOnInit();
+
+      expect(component.cols_buttons).toEqual(1);
+    }));
+
+    it('rows_buttons value should be 6 when getInnerWidth return a value greater than 480', fakeAsync(() => {
+      let width = 1000;
+      let spy_window = spyOn(component, 'getInnerWidth').and.returnValue(width);
+
+      component.ngOnInit();
+
+      expect(component.rows_buttons).toEqual(6);
+    }));
+
+    it('rows_buttons value should be 6 when getInnerWidth return a value equal to 480', fakeAsync(() => {
+      let width = 480;
+      let spy_window = spyOn(component, 'getInnerWidth').and.returnValue(width);
+
+      component.ngOnInit();
+
+      expect(component.rows_buttons).toEqual(6);
+    }));
+
+    it('rows_buttons value should be 1 when getInnerWidth return a value less than 480', fakeAsync(() => {
+      let width = 300;
+      let spy_window = spyOn(component, 'getInnerWidth').and.returnValue(width);
+
+      component.ngOnInit();
+
+      expect(component.rows_buttons).toEqual(1);
+    }));
+  });
+
+  describe('Methods called by template', () => {
+
+    it('onResize should set breakpoint value to 5 when getInnerWidth return a value greater than 480', fakeAsync(() => {
+      let width = 1000;
+      let spy_window = spyOn(component, 'getInnerWidth').and.returnValue(width);
+
+      component.onResize();
+
+      expect(component.breakpoint).toEqual(5);
+    }));
+
+    it('onResize should set breakpoint value to 5 when getInnerWidth return a value equal to 480', fakeAsync(() => {
+      let width = 480;
+      let spy_window = spyOn(component, 'getInnerWidth').and.returnValue(width);
+
+      component.onResize();
+
+      expect(component.breakpoint).toEqual(5);
+    }));
+
+    it('onResize should set breakpoint value to 1 when getInnerWidth return a value less than 480', fakeAsync(() => {
+      let width = 300;
+      let spy_window = spyOn(component, 'getInnerWidth').and.returnValue(width);
+
+      component.onResize();
+
+      expect(component.breakpoint).toEqual(1);
+    }));
+
+    it('onResize should set cols_tabs value to 3 when getInnerWidth return a value greater than 480', fakeAsync(() => {
+      let width = 1000;
+      let spy_window = spyOn(component, 'getInnerWidth').and.returnValue(width);
+
+      component.onResize();
+
+      expect(component.cols_tabs).toEqual(3);
+    }));
+
+    it('onResize should set cols_tabs value to 3 when getInnerWidth return a value equal to 480', fakeAsync(() => {
+      let width = 480;
+      let spy_window = spyOn(component, 'getInnerWidth').and.returnValue(width);
+
+      component.onResize();
+
+      expect(component.cols_tabs).toEqual(3);
+    }));
+
+    it('onResize should set cols_tabs value to 1 when getInnerWidth return a value less than 480', fakeAsync(() => {
+      let width = 300;
+      let spy_window = spyOn(component, 'getInnerWidth').and.returnValue(width);
+
+      component.onResize();
+
+      expect(component.cols_tabs).toEqual(1);
+    }));
+
+    it('onResize should set rows_tabs value to 6 when getInnerWidth return a value greater than 480', fakeAsync(() => {
+      let width = 1000;
+      let spy_window = spyOn(component, 'getInnerWidth').and.returnValue(width);
+
+      component.onResize();
+
+      expect(component.rows_tabs).toEqual(6);
+    }));
+
+    it('onResize should set rows_tabs value to 6 when getInnerWidth return a value equal to 480', fakeAsync(() => {
+      let width = 480;
+      let spy_window = spyOn(component, 'getInnerWidth').and.returnValue(width);
+
+      component.onResize();
+
+      expect(component.rows_tabs).toEqual(6);
+    }));
+
+    it('onResize should set rows_tabs value to 1 when getInnerWidth return a value less than 480', fakeAsync(() => {
+      let width = 300;
+      let spy_window = spyOn(component, 'getInnerWidth').and.returnValue(width);
+
+      component.onResize();
+
+      expect(component.rows_tabs).toEqual(1);
+    }));
+
+    it('onResize should set cols_buttons value to 2 when getInnerWidth return a value greater than 480', fakeAsync(() => {
+      let width = 1000;
+      let spy_window = spyOn(component, 'getInnerWidth').and.returnValue(width);
+
+      component.onResize();
+
+      expect(component.cols_buttons).toEqual(2);
+    }));
+
+    it('onResize should set cols_buttons value to 2 when getInnerWidth return a value equal to 480', fakeAsync(() => {
+      let width = 480;
+      let spy_window = spyOn(component, 'getInnerWidth').and.returnValue(width);
+
+      component.onResize();
+
+      expect(component.cols_buttons).toEqual(2);
+    }));
+
+    it('onResize should set cols_buttons value to 1 when getInnerWidth return a value less than 480', fakeAsync(() => {
+      let width = 300;
+      let spy_window = spyOn(component, 'getInnerWidth').and.returnValue(width);
+
+      component.onResize();
+
+      expect(component.cols_buttons).toEqual(1);
+    }));
+
+    it('onResize should set rows_buttons value to 6 when getInnerWidth return a value greater than 480', fakeAsync(() => {
+      let width = 1000;
+      let spy_window = spyOn(component, 'getInnerWidth').and.returnValue(width);
+
+      component.onResize();
+
+      expect(component.rows_buttons).toEqual(6);
+    }));
+
+    it('onResize should set rows_buttons value to 6 when getInnerWidth return a value equal to 480', fakeAsync(() => {
+      let width = 480;
+      let spy_window = spyOn(component, 'getInnerWidth').and.returnValue(width);
+
+      component.onResize();
+
+      expect(component.rows_buttons).toEqual(6);
+    }));
+
+    it('onResize should set rows_buttons value to 1 when getInnerWidth return a value less than 480', fakeAsync(() => {
+      let width = 300;
+      let spy_window = spyOn(component, 'getInnerWidth').and.returnValue(width);
+
+      component.onResize();
+
+      expect(component.rows_buttons).toEqual(1);
+    }));
+
+    it('setTabSelected should set tab_selected to empty string when parameter is an empty string', () => {
+      let textTab:string = '';
+
+      component.setTabSelected(textTab);
+
+      expect(component.tab_selected).toEqual(textTab);
+    })
+
+    it('setTabSelected should set tab_selected to string when parameter is a non-empty string', () => {
+      let textTab:string = 'Team';
+
+      component.setTabSelected(textTab);
+
+      expect(component.tab_selected).toEqual(textTab);
+    })
+  })
+
 });
