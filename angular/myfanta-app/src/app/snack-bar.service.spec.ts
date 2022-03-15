8a1e7d55672abdee5208b07c33e3b40770ebad21
Bellocci <francesco.bellocci@stud.unifi.it>
2022-03-15 12:00:02 +0100

    Defined openSnackBar method and related unit test.

--- angular/myfanta-app/src/app/snack-bar.service.spec.ts
+++ angular/myfanta-app/src/app/snack-bar.service.spec.ts
@@ -1,16 +1,60 @@
-import { TestBed } from '@angular/core/testing';
+import { fakeAsync, TestBed } from '@angular/core/testing';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 import { SnackBarService } from './snack-bar.service';
 
 describe('SnackBarService', () => {
   let service: SnackBarService;
+  let matSnackBar:MatSnackBar;
+
+  const mockMatSnackBar = {
+    open: () => {}
+  };
+
+  const mockStatus = {
+    default: ''
+  };
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [
+      ],
+      providers: [
+        SnackBarService,
+        { provide: MatSnackBar, useValue: mockMatSnackBar }, // <--- use mock
+      ]
+    });
     service = TestBed.inject(SnackBarService);
+    matSnackBar = TestBed.inject(MatSnackBar);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('should call MatSnackBar open method when openSnackBar method is called', fakeAsync(() => {
+    let textMessage:string = '';
+    let spy_snackbar = spyOn(matSnackBar, "open");
+
+    service.openSnackBar(textMessage);
+
+    expect(spy_snackbar).toHaveBeenCalledTimes(1);
+  }));
+
+  it('should call MatSnackBar open method with textMessage, "Close" and a config object', fakeAsync(() => {
+    let textMessage:string = 'SnackBar message';
+    let spy_snackbar = spyOn(matSnackBar, "open");
+
+    service.openSnackBar(textMessage);
+
+    const args = spy_snackbar.calls.argsFor(0);
+    expect(args.length).toBe(3);
+    expect(args[0]).toBe(textMessage);
+    expect(args[1]).toBe('Close');
+    expect(args[2]).toEqual({
+      duration : 3000,
+      horizontalPosition : 'center',
+      verticalPosition : 'bottom'
+    })
+  }))
 });
--- angular/myfanta-app/src/app/snack-bar.service.ts
+++ angular/myfanta-app/src/app/snack-bar.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
+import { MatSnackBar, MatSnackBarConfig } from '@angular/material/snack-bar';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SnackBarService {
 
-  constructor() { }
+  constructor(private _snackBar:MatSnackBar) { }
+  
+  private _action:string = 'Close';
+  private _config:MatSnackBarConfig = {
+    duration : 3 * 1000,
+    horizontalPosition : 'center',
+    verticalPosition : 'bottom'
+  }
+
+  openSnackBar(textMessage:string) {
+    this._snackBar.open(textMessage, this._action, this._config);
+  }
 }
