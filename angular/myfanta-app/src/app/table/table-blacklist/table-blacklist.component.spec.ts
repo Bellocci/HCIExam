04f676c54398a343eea4ff31930831b8a528f0cf
Bellocci <francesco.bellocci@stud.unifi.it>
2022-03-16 19:32:29 +0100

    Defined ngAfterViewInit method where istance dataSource. Defined unit test about all methods.

--- angular/myfanta-app/src/app/table/table-blacklist/table-blacklist.component.spec.ts
+++ angular/myfanta-app/src/app/table/table-blacklist/table-blacklist.component.spec.ts
@@ -1,10 +1,37 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, tick, fakeAsync } from '@angular/core/testing';
+import { InternalDimensionService } from 'src/app/internal-dimension.service';
+import { of } from 'rxjs';
 
 import { TableBlacklistComponent } from './table-blacklist.component';
+import { InternalDataService } from 'src/app/internal-data.service';
+import { MatTableDataSource } from '@angular/material/table';
+
+const BLACKLIST_PLAYERS = [
+  {
+    playerId : 1,
+    playerName : 'Musso',
+    team : 1,
+    cost : 12,
+    role : 'P',
+    age : 27,
+    matchPlayed : 24,
+  },
+  {
+    playerId : 2,
+    playerName : 'Toloi',
+    team : 1,
+    cost : 8,
+    role : 'D',
+    age : 31,
+    matchPlayed : 16,
+  }
+];
 
 describe('TableBlacklistComponent', () => {
   let component: TableBlacklistComponent;
   let fixture: ComponentFixture<TableBlacklistComponent>;
+  let internal_dimension: InternalDimensionService;
+  let internal_data: InternalDataService;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -16,10 +43,113 @@ describe('TableBlacklistComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(TableBlacklistComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+    internal_dimension = TestBed.inject(InternalDimensionService);
+    internal_data = TestBed.inject(InternalDataService);
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  describe('ngOnInit', () => {
+
+    it('testing subscribe method into getTableHeight is getting called', fakeAsync(() => {
+      let spy_height = spyOn(internal_dimension, 'getTableHeight').and.returnValue(of(''));
+      let spy_sub = spyOn(internal_dimension.getTableHeight(), 'subscribe');
+
+      component.ngOnInit();
+      tick();
+
+      expect(spy_height).toHaveBeenCalledBefore(spy_sub);
+      expect(spy_sub).toHaveBeenCalled();
+    }));
+
+    it('should call getTableHeight and get response a string', fakeAsync(() => {
+      let height:string = "400";
+      let spy_height = spyOn(internal_dimension, "getTableHeight").and.returnValue(of(height));
+
+      component.ngOnInit();
+      tick();
+
+      expect(spy_height).toHaveBeenCalled();
+      expect(component.height).toBe(height);
+    }));
+
+    it('should call getTableHeight and get response a string that contains only digit', fakeAsync(() => {
+      let height:string = "400";
+      let spy_height = spyOn(internal_dimension, "getTableHeight").and.returnValue(of(height));
+
+      component.ngOnInit();
+      tick();
+
+      expect(spy_height).toHaveBeenCalled();
+      expect(component.height).toMatch('^[0-9]*$');
+    }));
+
+    it('should call getTableHeight and if get a string that not contains only digit set default value', fakeAsync(() => {
+      let height:string = "400px";
+      let spy_height = spyOn(internal_dimension, "getTableHeight").and.returnValue(of(height));
+
+      component.ngOnInit();
+      tick();
+
+      expect(spy_height).toHaveBeenCalled();
+      expect(component.height).toBe(component.getDefaultHeight());
+    }));
+
+    it('testing subscribe method into getTableWidth is getting called', fakeAsync(() => {
+      let spy_width = spyOn(internal_dimension, 'getTableWidth').and.returnValue(of(''));
+      let spy_sub = spyOn(internal_dimension.getTableWidth(), 'subscribe');
+
+      component.ngOnInit();
+      tick();
+
+      expect(spy_width).toHaveBeenCalledBefore(spy_sub);
+      expect(spy_sub).toHaveBeenCalled();
+    }));
+
+    it('should call getTableWidth and get response a string', fakeAsync(() => {
+      let width:string = "400";
+      let spy_width = spyOn(internal_dimension, "getTableWidth").and.returnValue(of(width));
+
+      component.ngOnInit();
+      tick();
+
+      expect(spy_width).toHaveBeenCalled();
+      expect(component.width).toBe(width);
+    }));
+
+    it('should call getTableWidth and if get a string that not contains only digit set default value', fakeAsync(() => {
+      let width:string = "400px";
+      let spy_width = spyOn(internal_dimension, "getTableWidth").and.returnValue(of(width));
+
+      component.ngOnInit();
+      tick();
+
+      expect(spy_width).toHaveBeenCalled();
+      expect(component.width).toBe(component.getDefaultWidth());
+    }));
+
+  });
+
+  describe('ngAfterViewInit', () => {
+    it('testing subscribe method from internal_data.getBlacklistPlayers is getting called', fakeAsync(() => {
+      let spy_blacklist = spyOn(internal_data, "getBlacklistPlayers").and.returnValue(of([]));
+      let spy_sub = spyOn(internal_data.getBlacklistPlayers(), "subscribe");
+
+      component.ngAfterViewInit();
+
+      expect(spy_blacklist).toHaveBeenCalledBefore(spy_sub);
+      expect(spy_sub).toHaveBeenCalled();
+    }));
+
+    it('should set property dataSource.data with array', fakeAsync(() => {
+      let spy_blacklist = spyOn(internal_data, "getBlacklistPlayers").and.returnValue(of(BLACKLIST_PLAYERS));
+
+      component.ngAfterViewInit();
+
+      expect(spy_blacklist).toHaveBeenCalled();
+      expect(component.dataSource.data).toEqual(BLACKLIST_PLAYERS);
+    }));
   });
 });
--- angular/myfanta-app/src/app/table/table-blacklist/table-blacklist.component.ts
+++ angular/myfanta-app/src/app/table/table-blacklist/table-blacklist.component.ts
@@ -1,15 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
+import { InternalDataService } from 'src/app/internal-data.service';
+import { InternalDimensionService } from 'src/app/internal-dimension.service';
+import { TableComponent } from '../table.component';
 
 @Component({
   selector: 'app-table-blacklist',
-  templateUrl: './table-blacklist.component.html',
-  styleUrls: ['./table-blacklist.component.css']
+  templateUrl: '../table.component.html',
+  styleUrls: ['../table.component.css']
 })
-export class TableBlacklistComponent implements OnInit {
+export class TableBlacklistComponent extends TableComponent implements AfterViewInit {
 
-  constructor() { }
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
 
-  ngOnInit(): void {
+  constructor(
+    private internal_dimension_service:InternalDimensionService,
+    private internal_data_service:InternalDataService) 
+  {
+    super(internal_dimension_service);
   }
 
+  ngAfterViewInit(): void {
+    this.internal_data_service.getBlacklistPlayers().subscribe(blacklist => {
+      this.dataSource = new MatTableDataSource(blacklist);
+      this.dataSource.paginator = this.paginator;
+      this.dataSource.sort = this.sort;
+    });
+  }
 }
