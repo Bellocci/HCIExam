ef21c84c7ef597c8dc28a0daf295e379236009b2
Bellocci <francesco.bellocci@stud.unifi.it>
2022-03-16 17:56:32 +0100

    Override ngAfterInit and create getTeam and getTeamWithFavoritPlayers methods. Write unit test for all methods.
    Override dataSource, columnsToDisplay, paginator and sort.
    
    Defined into constructor internal_dimension_service and internal_data_service variables.

--- angular/myfanta-app/src/app/table/table-empty/table-empty.component.spec.ts
+++ angular/myfanta-app/src/app/table/table-empty/table-empty.component.spec.ts
@@ -1,14 +1,50 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, fakeAsync, TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { of } from 'rxjs';
 
+import { InternalDataService } from 'src/app/internal-data.service';
 import { TableEmptyComponent } from './table-empty.component';
+import { MatTableDataSource } from '@angular/material/table';
+
+const GENERATE_PLAYERS_LIST = [
+  {
+    playerId : 1,
+    playerName : 'Musso',
+    team : 1,
+    cost : 12,
+    role : 'P',
+    age : 27,
+    matchPlayed : 24,
+  },
+  {
+    playerId : 2,
+    playerName : 'Toloi',
+    team : 1,
+    cost : 8,
+    role : 'D',
+    age : 31,
+    matchPlayed : 16,
+  }
+];
 
 describe('TableEmptyComponent', () => {
   let component: TableEmptyComponent;
   let fixture: ComponentFixture<TableEmptyComponent>;
+  let internal_data: InternalDataService;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ TableEmptyComponent ]
+      imports: [
+        HttpClientTestingModule,
+        MatPaginatorModule,
+      ],
+      declarations: [ 
+        TableEmptyComponent
+      ],
+      providers: [
+        InternalDataService
+      ]
     })
     .compileComponents();
   });
@@ -16,10 +52,85 @@ describe('TableEmptyComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(TableEmptyComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+    internal_data = TestBed.inject(InternalDataService);
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  describe('ngAfterViewInit', () => {
+    it('should define an empty dataSource', () => {
+      expect(component.dataSource).not.toBeDefined();
+
+      component.ngAfterViewInit();
+
+      expect(component.dataSource.data.length).toBe(0);
+    });
+
+    it('should set property dataSource.paginator with paginator', () => {
+      component.ngAfterViewInit();
+
+      expect(component.dataSource.paginator).toEqual(component.paginator);
+    });
+
+    it('should set property dataSource.sort with sort', () => {
+      component.ngAfterViewInit();
+
+      expect(component.dataSource.sort).toEqual(component.sort);
+    });
+
+    it('should set property dataSource.data with empty array', () => {
+      component.ngAfterViewInit();
+
+      expect(component.dataSource.data).toEqual([]);
+    });
+  });
+
+  describe('methods call from template', () => {
+
+    beforeEach(() => {
+      component.dataSource = new MatTableDataSource();
+      component.dataSource.data = [];
+    });
+
+    it('testing generateTeam and subscribe methods into getTeam are getting called', fakeAsync(() => {
+      let spy_generate_team = spyOn(internal_data, "generateTeam").and.returnValue(of([]));
+      let spy_sub = spyOn(internal_data.generateTeam(), "subscribe");
+
+      component.getTeam();
+      
+      expect(spy_generate_team).toHaveBeenCalledBefore(spy_sub);
+      expect(spy_sub).toHaveBeenCalled();
+    }));
+
+    it('should call getTeam and set dataSource with array', fakeAsync(() => {
+      let spy_generate_team = spyOn(internal_data, "generateTeam").and.returnValue(of(GENERATE_PLAYERS_LIST));
+
+      component.getTeam();
+
+      expect(spy_generate_team).toHaveBeenCalled();
+      expect(component.dataSource.data).toEqual(GENERATE_PLAYERS_LIST);
+    }));
+
+    it('testing generateTeamWithFavoritList and subscribe methods into getTeamWithFavoritPlayers are getting called',
+      fakeAsync(() => {
+        let spy_generate_favorit = spyOn(internal_data, "generateTeamWithFavoritList").and.returnValue(of([]));
+        let spy_sub = spyOn(internal_data.generateTeamWithFavoritList(), "subscribe");
+
+        component.getTeamWithFavoritPlayers();
+
+        expect(spy_generate_favorit).toHaveBeenCalledBefore(spy_sub);
+        expect(spy_sub).toHaveBeenCalled();
+    }));
+
+    it('should call getTeamWithFavoritPlayers and set dataSource with array', fakeAsync(() => {
+      let spy_generate_favorit = spyOn(internal_data, "generateTeamWithFavoritList").and.returnValue(of(GENERATE_PLAYERS_LIST));
+
+      component.getTeamWithFavoritPlayers();
+
+      expect(spy_generate_favorit).toHaveBeenCalled();
+      expect(component.dataSource.data).toEqual(GENERATE_PLAYERS_LIST);
+    }));
   });
 });
--- angular/myfanta-app/src/app/table/table-empty/table-empty.component.ts
+++ angular/myfanta-app/src/app/table/table-empty/table-empty.component.ts
@@ -1,4 +1,8 @@
-import { AfterViewInit, Component } from '@angular/core';
+import { AfterViewInit, Component, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
+import { InternalDataService } from 'src/app/internal-data.service';
 
 import { InternalDimensionService } from 'src/app/internal-dimension.service';
 import { TableComponent } from '../table.component';
@@ -11,10 +15,41 @@ import { TableComponent } from '../table.component';
 })
 export class TableEmptyComponent extends TableComponent implements AfterViewInit {
 
-  constructor(private internal_dimension_service:InternalDimensionService) {
+  override dataSource!: MatTableDataSource<any>;
+  override columnsToDisplay = ['Name', 'Team', 'Cost'];
+
+  @ViewChild(MatPaginator) 
+  override paginator!:MatPaginator;
+
+  @ViewChild(MatSort)
+  override sort!:MatSort;
+
+  constructor(
+    private internal_dimension_service:InternalDimensionService,
+    private internal_data_service:InternalDataService
+    ) 
+  {
     super(internal_dimension_service);
   }
+  
+  override ngAfterViewInit(): void {
+    this.dataSource = new MatTableDataSource();
+    this.dataSource.data = [];
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
+  }
 
+  getTeam() {
+    this.internal_data_service.generateTeam().subscribe(players => {
+      this.dataSource.data = players;
+    });
+  }
+
+  getTeamWithFavoritPlayers() {
+    this.internal_data_service.generateTeamWithFavoritList().subscribe(favorit_players => {
+      this.dataSource.data = favorit_players;
+    });
+  }
 }
 
 
