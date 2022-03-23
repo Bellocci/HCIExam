ee602aaa92a947f7020fd215afd8330a36841678
Bellocci <francesco.bellocci@stud.unifi.it>
2022-03-23 18:02:43 +0100

    Removed unit test about window resize

--- angular/myfanta-app/src/app/toolbar/toolbar-base/toolbar-base.component.spec.ts
+++ angular/myfanta-app/src/app/toolbar/toolbar-base/toolbar-base.component.spec.ts
@@ -39,41 +39,6 @@ describe('ToolbarBaseComponent', () => {
   });
 
   describe('Template methods', () => {
-    
-    it('should call isMobileLayout method when window is resized', () => {
-      const spy_isMobile = spyOn(component, "isMobileLayout");
-  
-      window.dispatchEvent(new Event('resize'));
-  
-      expect(spy_isMobile).toHaveBeenCalled();
-    });
-  
-    it('should isMobileLayout method set is_mobile to false when window is resized to width greater than 800', () => {
-      component.is_mobile = true;
-      window.innerWidth = 1000;
-  
-      window.dispatchEvent(new Event('resize'));
-  
-      expect(component.is_mobile).toBeFalse();
-    });
-  
-    it('should isMobileLayout method set is_mobile to false when window is resized to width equal to 800', () => {
-      component.is_mobile = true;
-      window.innerWidth = 800;
-  
-      window.dispatchEvent(new Event('resize'));
-  
-      expect(component.is_mobile).toBeFalse();
-    });
-  
-    it('should isMobileLayout method set is_mobile to true when window is resized to width less than 800', () => {
-      component.is_mobile = false;
-      window.innerWidth = 700;
-  
-      window.dispatchEvent(new Event('resize'));
-  
-      expect(component.is_mobile).toBeTrue();
-    });
   
     it('should emit signal when openSidenavFromChild method is called', () => {
       let spy_emit = spyOn(component.sidenav_emit, "emit");
