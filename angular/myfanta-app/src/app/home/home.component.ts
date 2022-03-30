6508635566a04a819c8c558af7acfde69006ef5b
Bellocci <francesco.bellocci@stud.unifi.it>
2022-03-30 19:03:30 +0200

    Reduced animation time for open and close championship list

--- angular/myfanta-app/src/app/home/home.component.ts
+++ angular/myfanta-app/src/app/home/home.component.ts
@@ -21,11 +21,11 @@ import { SharedService } from '../shared.service';
       transition('closeList => openList', [
         group([
           animate(
-            "400ms cubic-bezier(0.4, 0, 0.2, 1)", 
+            "300ms cubic-bezier(0.4, 0, 0.2, 1)", 
             style({height: '*'})
           ),
           animate(
-            "350ms cubic-bezier(0.4, 0, 0.2, 1)",
+            "250ms cubic-bezier(0.4, 0, 0.2, 1)",
             style({opacity: '1'})
           ),
         ])
@@ -33,11 +33,11 @@ import { SharedService } from '../shared.service';
       transition('openList => closeList', [
         group([
           animate(
-            "400ms cubic-bezier(0.4, 0, 0.2, 1)", 
+            "300ms cubic-bezier(0.4, 0, 0.2, 1)", 
             style({height: '0'})
           ),
           animate(
-            "200ms cubic-bezier(0.4, 0, 0.2, 1)",
+            "250ms cubic-bezier(0.4, 0, 0.2, 1)",
             style({opacity: '0'})
           ),
         ])
@@ -51,7 +51,7 @@ import { SharedService } from '../shared.service';
         display: 'none',
       })),
       transition('* => *', [
-          animate('400ms cubic-bezier(0.4, 0, 0.2, 1)')
+          animate('300ms cubic-bezier(0.4, 0, 0.2, 1)')
       ])
     ])
   ]
