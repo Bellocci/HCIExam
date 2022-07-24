87f018a405ba4011e230fd9c6104a6db729b7722
Bellocci <francesco.bellocci@stud.unifi.it>
2022-07-24 18:03:08 +0200

    Updated TableComponent.
    # html
    
    - created mat-table with column "Name", "Team", "Cost" and "Favorite Player"
    - getted datasource from getDataSource method.
    - displayed button with icon favorite in "Favorite Player" column.
    - added mat-paginator.
    
    # css
    
    - added class table that define width of table.
    - added class mat-header-cell that define padding left and font-weight
      for header.
    - added class .mat-cell that define padding left and border for each cell
      of the table.
    - defined width for each column of table.
    - modified link view.
    
    # ts
    
    - created subscribePlayerList method that keep the value of _player_list
      variable update with value returned by getPlayerList method from team
      data service.
    - Created methods to add, remove and search for a player in the list of
      favorite players by invoking the methods in the team data service.
    - created handlePageEvent method for update value of pageIndex and pagesize
      in mat-paginator.
    
    # spec.ts
    
    - add test to verify that getPlyerList from team data service is subscribed.
    - add test to verify that datasource.data value is setted with value
      returned by getPlayerList method from team data service.
    - add test that verify dataSource.paginator and dataSource.sort are setted.
    - add test to verify that isFavoritePlayer method call
      isPlayerIntoFavoriteList method from team data service.
    - add test to verify that setPlayerAsFavorite method call
      addPlayerIntoFavoriteList method from team data service.
    - add test to verify that removePlayerAsFavorite method call
      removePlayerFromFavoriteList method from team data service.
    - add tests to verify that pageIndex and pageSize values are setted
      with value passed as argument into handlePageEvent method.
    
    # dom.spec.ts
    
    - created file for testing dom.
    - added tests to verify component of table.
    - added test to verify that table has paginator.
    - added test to verify that table has sort.
    - added test to verify that setPlayerAsFavorite method is called when
      button with icon favorite_border is clicked.
    - added test to verify that removePlayerAsFavorite method is called when
      button with icon favorite is clicked.
    - added tests to verify that paginator is able to navigate between pages,
      go to the last page and come back to first page and set page size.
    - added test to verify that sort is able to get the sorted direction
      of a header.
    - added test to verify that navigate to /detail/[player_name] when player
      name is clicked.

--- angular/myfanta-app/src/app/table/table.component.spec.ts
+++ angular/myfanta-app/src/app/table/table.component.spec.ts
@@ -1,32 +1,24 @@
-import { waitForAsync, ComponentFixture, TestBed, fakeAsync, tick } from '@angular/core/testing';
-import { HttpClientTestingModule } from '@angular/common/http/testing';
-
-import { NoopAnimationsModule } from '@angular/platform-browser/animations';
-import { MatPaginatorModule } from '@angular/material/paginator';
-import { MatSortModule } from '@angular/material/sort';
-import { MatTableModule } from '@angular/material/table';
+import { waitForAsync, ComponentFixture, TestBed } from '@angular/core/testing';
+import { PageEvent } from '@angular/material/paginator';
+import { of } from 'rxjs';
+import { TeamDataService } from '../service/team-data.service';
 
 import { TableComponent } from './table.component';
-import { InternalDimensionService } from '../service/internal-dimension.service';
-import { of } from 'rxjs';
 
 describe('TableComponent', () => {
   let component: TableComponent;
   let fixture: ComponentFixture<TableComponent>;
-  let internal_dim_service:InternalDimensionService;
+  let team_data: TeamDataService;
 
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
-      declarations: [ TableComponent ],
       imports: [
-        NoopAnimationsModule,
-        MatPaginatorModule,
-        MatSortModule,
-        MatTableModule,
-        HttpClientTestingModule
+      ],
+      declarations: [ 
+        TableComponent 
       ],
       providers: [
-        InternalDimensionService
+        TeamDataService
       ]
     }).compileComponents();
   }));
@@ -34,7 +26,7 @@ describe('TableComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(TableComponent);
     component = fixture.componentInstance;
-    internal_dim_service = TestBed.inject(InternalDimensionService);
+    team_data = TestBed.inject(TeamDataService);
   });
 
   it('should compile', () => {
@@ -43,82 +35,222 @@ describe('TableComponent', () => {
 
   describe('ngOnInit', () => {
 
-    it('testing subscribe method into getTableHeight is getting called', fakeAsync(() => {
-      let spy_height = spyOn(internal_dim_service, 'getTableHeight').and.returnValue(of(''));
-      let spy_sub = spyOn(internal_dim_service.getTableHeight(), 'subscribe');
+    it('testing subscribe method into getPlayerList method from team_data service', () => {
+      const spy_playerList = spyOn(team_data, "getPlayerList").and.returnValue(of());
+      const spy_subscribe = spyOn(team_data.getPlayerList(), "subscribe");
 
       component.ngOnInit();
-      tick();
 
-      expect(spy_height).toHaveBeenCalledBefore(spy_sub);
-      expect(spy_sub).toHaveBeenCalled();
-    }));
+      expect(spy_playerList).toHaveBeenCalledBefore(spy_subscribe);
+      expect(spy_subscribe).toHaveBeenCalled();
+    });
 
-    it('should call getTableHeight and get response a string', fakeAsync(() => {
-      let height:string = "400";
-      let spy_height = spyOn(internal_dim_service, "getTableHeight").and.returnValue(of(height));
+    it('should set datasource data with value returned from getPlayerList method of team_data service', () => {
+      const spy_playerList = spyOn(team_data, "getPlayerList").and.returnValue(of(PLAYER_DATA));
+      component.ngOnInit();
+      component.getDataSource().data = [];
 
       component.ngOnInit();
-      tick();
 
-      expect(spy_height).toHaveBeenCalled();
-      expect(component.height).toBe(height);
-    }));
+      expect(component.getDataSource().data.length).toBe(PLAYER_DATA.length);
+    });
+  });
 
-    it('should call getTableHeight and get response a string that contains only digit', fakeAsync(() => {
-      let height:string = "400";
-      let spy_height = spyOn(internal_dim_service, "getTableHeight").and.returnValue(of(height));
-
+  describe('ngAfterViewInit', () => {
+    
+    it('should set _dataSource.paginator with _paginator', () => {
       component.ngOnInit();
-      tick();
+      component.getDataSource().paginator = null;
 
-      expect(spy_height).toHaveBeenCalled();
-      expect(component.height).toMatch('^[0-9]*$');
-    }));
+      component.ngAfterViewInit();
 
-    it('should call getTableHeight and if get a string that not contains only digit set default value', fakeAsync(() => {
-      let height:string = "400px";
-      let spy_height = spyOn(internal_dim_service, "getTableHeight").and.returnValue(of(height));
+      expect(component.getDataSource().paginator).toEqual(component['_paginator']);
+    });
 
+    it('should set _dataSource.sort with _sort', () => {
       component.ngOnInit();
-      tick();
+      component.getDataSource().sort = null;
 
-      expect(spy_height).toHaveBeenCalled();
-      expect(component.height).toBe(component.getDefaultHeight());
-    }));
+      component.ngAfterViewInit();
 
-    it('testing subscribe method into getTableWidth is getting called', fakeAsync(() => {
-      let spy_width = spyOn(internal_dim_service, 'getTableWidth').and.returnValue(of(''));
-      let spy_sub = spyOn(internal_dim_service.getTableWidth(), 'subscribe');
+      expect(component.getDataSource().sort).toEqual(component['_sort']);
+    })
+  });
 
-      component.ngOnInit();
-      tick();
+  describe('Methods from template', () => {
 
-      expect(spy_width).toHaveBeenCalledBefore(spy_sub);
-      expect(spy_sub).toHaveBeenCalled();
-    }));
+    it('should call isPlayerIntoFavoriteList method from team data service when isFavoritePlayer method is called', () => {
+      const spy_favoriteTeam = spyOn(team_data, "isPlayerIntoFavoriteList");
+      const player = PLAYER_DATA[0];
 
-    it('should call getTableWidth and get response a string', fakeAsync(() => {
-      let width:string = "400";
-      let spy_width = spyOn(internal_dim_service, "getTableWidth").and.returnValue(of(width));
+      component.isFavoritePlayer(player)
 
-      component.ngOnInit();
-      tick();
+      expect(spy_favoriteTeam).toHaveBeenCalledWith(player);
+    });
 
-      expect(spy_width).toHaveBeenCalled();
-      expect(component.width).toBe(width);
-    }));
+    it('should isFavoritePlayer method return the value returned by isPlayerIntoFavoriteList method from team data service', 
+    () =>{
+      const spy_favoriteTeam = spyOn(team_data, "isPlayerIntoFavoriteList").and.returnValue(false);
+      const player = PLAYER_DATA[0];
 
-    it('should call getTableWidth and if get a string that not contains only digit set default value', fakeAsync(() => {
-      let width:string = "400px";
-      let spy_width = spyOn(internal_dim_service, "getTableWidth").and.returnValue(of(width));
+      expect(component.isFavoritePlayer(player)).toBeFalse();
 
-      component.ngOnInit();
-      tick();
+      spy_favoriteTeam.and.returnValue(true);
 
-      expect(spy_width).toHaveBeenCalled();
-      expect(component.width).toBe(component.getDefaultWidth());
-    }));
+      expect(component.isFavoritePlayer(player)).toBeTrue();
+    });
 
+    it('should call addPlayerIntoFavoriteList method from team data service when setPlayerAsFavorite method is called',
+    () => {
+      const spy_favoriteTeam = spyOn(team_data, "addPlayerIntoFavoriteList");
+      const player = PLAYER_DATA[0];
+
+      component.setPlayerAsFavorite(player);
+
+      expect(spy_favoriteTeam).toHaveBeenCalledOnceWith(player);
+    });
+
+    it('should call removePlayerFromFavoriteList method from team data service when removePlayerAsFavorite method is called',
+    () => {
+      const spy_favoriteTeam = spyOn(team_data, "removePlayerFromFavoriteList");
+      const player = PLAYER_DATA[0];
+
+      component.removePlayerAsFavorite(player);
+
+      expect(spy_favoriteTeam).toHaveBeenCalledOnceWith(player);
+    });
+
+    it('should set _pageIndex with value from PageEvent when handlePageEvent method is called', () => {
+      const event = new PageEvent();
+      event.pageIndex = 1;
+      const default_value = 0
+      expect(component.getPageIndex()).toBe(default_value);
+      
+      component.handlePageEvent(event);
+
+      expect(component.getPageIndex()).toBe(1);
+    });
+
+    it('should set _pageSize with value from PageEvent when handlePageEvent method is called', () => {
+      const event = new PageEvent();
+      event.pageSize = 20;
+      const default_value = 10;
+      expect(component.getPageSize()).toBe(default_value);
+      
+      component.handlePageEvent(event);
+
+      expect(component.getPageSize()).toBe(20);
+    });
   });
 });
+
+const PLAYER_DATA = [
+  {
+      playerId : 1,
+      playerName : 'Musso',
+      teamName : 'Atalanta',
+      cost : 12,
+      role : 'P',
+      age : 27,
+      matchPlayed : 24,
+  },
+  {
+      playerId : 2,
+      playerName : 'Toloi',
+      teamName : 'Atalanta',
+      cost : 8,
+      role : 'D',
+      age : 31,
+      matchPlayed : 16,
+  },
+  {
+      playerId : 3,
+      playerName : 'Malinovskyi',
+      teamName : 'Atalanta',
+      cost : 22,
+      role : 'C',
+      age : 28,
+      matchPlayed : 22,
+  },
+  {
+      playerId : 4,
+      playerName : 'Pasalic',
+      teamName : 'Atalanta',
+      cost : 28,
+      role : 'C',
+      age : 27,
+      matchPlayed : 24,
+  },
+  {
+      playerId : 5,
+      playerName : 'Arnautovic',
+      teamName : 'Bologna',
+      cost : 24,
+      role : 'A',
+      age : 32,
+      matchPlayed : 23,
+  },
+  {
+      playerId : 6,
+      playerName : 'De Silvestri',
+      teamName : 'Bologna',
+      cost : 14,
+      role : 'D',
+      age : 33,
+      matchPlayed : 23,
+  },
+  {
+      playerId : 7,
+      playerName : 'Soriano',
+      teamName : 'Bologna',
+      cost : 10,
+      role : 'C',
+      age : 31,
+      matchPlayed : 25,
+  },
+  {
+      playerId : 8,
+      playerName : 'Skorupski',
+      teamName : 'Bologna',
+      cost : 10,
+      role : 'P',
+      age : 30,
+      matchPlayed : 27,
+  },
+  {
+      playerId : 9,
+      playerName : 'Theate',
+      teamName : 'Bologna',
+      cost : 12,
+      role : 'D',
+      age : 21,
+      matchPlayed : 22,
+  },
+  {
+      playerId : 10,
+      playerName : 'Orsolini',
+      teamName : 'Bologna',
+      cost : 18,
+      role : 'C',
+      age : 25,
+      matchPlayed : 18,
+  },
+  {
+      playerId : 11,
+      playerName : 'Muriel',
+      teamName : 'Atalanta',
+      cost : 23,
+      role : 'A',
+      age : 30,
+      matchPlayed : 16,
+  },
+  {
+      playerId : 12,
+      playerName : 'Zapata',
+      teamName : 'Atalanta',
+      cost : 34,
+      role : 'A',
+      age : 30,
+      matchPlayed : 17,
+  },
+];
