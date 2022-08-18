29313fc403888e838a8003c7919e8c35a1226af3
Bellocci <francesco.bellocci@stud.unifi.it>
2022-08-18 10:39:28 +0200

    Updated PlayerList component.
    # html
    
    - Added buttons for filter player about roles.
    - Added chips for filter player about teams.
    - Added form-field to filter player by names.
    
    # css
    
    - Added class for buttons and chips styles.
    - Added class for form-field style.
    
    # ts
    
    - Updated _search_player pipe method.
    - Added getTeamName method into ngOnInit where push name of team and
      boolean in _teams variable.
    - Added getter method for window.innerWidth, _roles and _teams.
    - Added selectedRole method that given name of role, toggle selected
      key into _roles.
    - Added isRoleSelected method that return true if selected key into _roles
      is true, false otherwhise.
    - Added selectedTeam method that given name of team toggle selected
      key into _teams.
    - Added isTeamSelected method that return true if selected key into _teams
      is true, false otherwhise.
    - Added isShortSelectedTeam method that return true if window.width is less
      or equal than 600px, false otherwhise.
    - Added filterPlayerByRole method that call filterPlayerByRoles method
      from team data service with role.name and role.selected as arguments.
    - Added filterPLayerByTeam method that call filterPlayersByTeam method
      from team data service with team.name and team.selected as arguments.
    
    # dom.spec.ts
    
    - Added tests to verify that buttons are showed.
    - Added tests to verify that chips are showed.
    - Added tests to verify that selected is showed.
    - Added test to veify that buttons call selectedRole method when they are
      clicked.
    - Added test to verify that chips call selectedTeam method when they are
      clicked.
    - Added test to verify that options call selectedTeam method when they
      are clicked.
    
    # spec.ts
    
    - Added tests for ngOnInit method.
    - Added tests for selectedRole method.
    - Added tests for isRoleSelected method.
    - Added tests for selectedTeam method.
    - Added tests for isSelectedTeam method.
    - Added tests for isShortSelectedTeam method.

--- angular/myfanta-app/src/app/player-list/player-list.component.dom.spec.ts
+++ angular/myfanta-app/src/app/player-list/player-list.component.dom.spec.ts
@@ -1,13 +1,17 @@
 import { HarnessLoader } from "@angular/cdk/testing";
 import { TestbedHarnessEnvironment } from "@angular/cdk/testing/testbed";
-import { ComponentFixture, fakeAsync, TestBed } from "@angular/core/testing";
-import { FormsModule } from "@angular/forms";
+import { async, ComponentFixture, fakeAsync, TestBed, tick } from "@angular/core/testing";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 import { NoopAnimationsModule } from "@angular/platform-browser/animations";
 import { MaterialModule } from "../material-module";
 import { PlayerListComponent } from "./player-list.component";
-import {MatFormFieldHarness} from '@angular/material/form-field/testing';
+import { MatFormFieldHarness } from '@angular/material/form-field/testing';
 import { By } from "@angular/platform-browser";
 import { MatInputHarness } from "@angular/material/input/testing";
+import { MatButtonHarness } from "@angular/material/button/testing";
+import { MatChipHarness, MatChipListboxHarness } from '@angular/material/chips/testing';
+import { Component } from "@angular/core";
+import { MatSelectHarness } from "@angular/material/select/testing";
 
 
 
@@ -22,9 +26,11 @@ describe('PlayerListComponent DOM', () => {
                 MaterialModule,
                 NoopAnimationsModule,
                 FormsModule,
+                ReactiveFormsModule
             ],
             declarations: [ 
-                PlayerListComponent 
+                PlayerListComponent,
+                MockTableComponent
             ]
         }).compileComponents();
     });
@@ -42,10 +48,89 @@ describe('PlayerListComponent DOM', () => {
 
     describe('Template', () => {
 
-        it('should have form-field', fakeAsync(async () => {
+        it('should have button with text "P"', fakeAsync(async () => {
+            const buttons = await loader.getAllHarnesses(MatButtonHarness.with({text : 'P'}));
+
+            expect(buttons.length).toBe(1);
+        }));
+
+        it('should have button with text "D"', fakeAsync(async () => {
+            const buttons = await loader.getAllHarnesses(MatButtonHarness.with({text : 'D'}));
+
+            expect(buttons.length).toBe(1);
+        }));
+
+        it('should have button with text "C"', fakeAsync(async () => {
+            const buttons = await loader.getAllHarnesses(MatButtonHarness.with({text : 'C'}));
+
+            expect(buttons.length).toBe(1);
+        }));
+
+        it('should have button with text "A"', fakeAsync(async () => {
+            const buttons = await loader.getAllHarnesses(MatButtonHarness.with({text : 'A'}));
+
+            expect(buttons.length).toBe(1);
+        }));
+
+        it('should have a mat-chip-list when isShortSelectedTeam method return false', fakeAsync(async () => {
+            const spy_short_select = spyOn(component, "isShortSelectedTeam").and.returnValue(false);
+            fixture.detectChanges();
+            const chipList = await loader.getAllHarnesses(MatChipListboxHarness);
+
+            expect(chipList.length).toBe(1);
+        }));
+
+        it('should mat-chip-list have a mat-chip for each team returned by getTeam method', fakeAsync(async () => {
+            const spy_short_select = spyOn(component, "isShortSelectedTeam").and.returnValue(false);
+            const spy_team = spyOn(component, "getTeam").and.returnValue(TEAM_DICT);
+            fixture.detectChanges();
+            const chipList = await loader.getHarness(MatChipListboxHarness);
+            
+            const chips = await chipList.getChips();
+
+            expect(chips.length).toBe(TEAMS.length);
+            expect(await chips[0].getText()).toEqual(TEAMS[0]);
+            expect(await chips[1].getText()).toEqual(TEAMS[1]);
+            expect(await chips[2].getText()).toEqual(TEAMS[2]);
+        }));
+
+        it('should have a select when isShortSelectedTeam method return true', fakeAsync(async () => {
+            const spy_short_select = spyOn(component, "isShortSelectedTeam").and.returnValue(true);
+            fixture.detectChanges();
+            const select = await loader.getAllHarnesses(MatSelectHarness);
+
+            expect(select.length).toBe(1);
+        }));
+
+        it('should select has an option for each team returned by getTeam method', fakeAsync(async () => {
+            const spy_short_select = spyOn(component, "isShortSelectedTeam").and.returnValue(true);
+            const spy_team = spyOn(component, "getTeam").and.returnValue(TEAM_DICT);
+            fixture.detectChanges();
+            const select = await loader.getHarness(MatSelectHarness);
+
+            await select.open();
+            const options = await select.getOptions();
+
+            expect(options.length).toBe(TEAMS.length);
+            expect(await options[0].getText()).toEqual(TEAMS[0]);
+            expect(await options[1].getText()).toEqual(TEAMS[1]);
+            expect(await options[2].getText()).toEqual(TEAMS[2]);
+        }));
+
+        it('should have a form-field when isShortSelectedTeam method return false', fakeAsync(async () => {
+            const spy_short_select = spyOn(component, "isShortSelectedTeam").and.returnValue(false);
+            fixture.detectChanges();
             const form_field = await loader.getAllHarnesses(MatFormFieldHarness);
 
             expect(form_field.length).toBe(1);
+        }));
+
+        it('should have two form-field when isShortSelectedTeam method return true', fakeAsync(async () => {
+            const spy_short_select = spyOn(component, "isShortSelectedTeam").and.returnValue(true);
+            fixture.detectChanges();
+            const form_field = await loader.getAllHarnesses(MatFormFieldHarness);
+
+            expect(form_field.length).toBe(2);
         }));
 
         it('should have app-table component', () => {
@@ -57,10 +142,72 @@ describe('PlayerListComponent DOM', () => {
 
     describe('Template methods', () => {
 
+        it('should buttons about player role call selectedRole method when they are clicked', fakeAsync(async () => {
+            const spy_selected = spyOn(component, "selectedRole");
+            const btn_p = await loader.getHarness(MatButtonHarness.with({text : ROLES[0]}));
+            const btn_d = await loader.getHarness(MatButtonHarness.with({text : ROLES[1]}));
+            const btn_c = await loader.getHarness(MatButtonHarness.with({text : ROLES[2]}));
+            const btn_a = await loader.getHarness(MatButtonHarness.with({text : ROLES[3]}));
+
+            await btn_p.click();
+            expect(spy_selected).toHaveBeenCalledWith(ROLES[0]);
+
+            await btn_d.click();
+            expect(spy_selected).toHaveBeenCalledWith(ROLES[1]);
+
+            await btn_c.click();
+            expect(spy_selected).toHaveBeenCalledWith(ROLES[2]);
+
+            await btn_a.click();
+            expect(spy_selected).toHaveBeenCalledWith(ROLES[3]);
+        }));
+
+        it('should mat-chips call selectedTeam method when they are clicked', fakeAsync(async () => {
+            const spy_team = spyOn(component, "getTeam").and.returnValue(TEAM_DICT);
+            const spy_selected = spyOn(component, "selectedTeam");
+            fixture.detectChanges();
+
+            const chips = fixture.debugElement.queryAll(By.css('mat-chip'));
+            chips[1].nativeElement.click();
+
+            expect(spy_selected).toHaveBeenCalledTimes(1);
+            expect(spy_selected).toHaveBeenCalledWith(TEAMS[1]);
+        }));
+
+        it('should call selectedTeam method with argument name of team selected when option in selected is clicked', 
+        fakeAsync(async () => {
+            const spy_short_select = spyOn(component, "isShortSelectedTeam").and.returnValue(true);
+            const spy_team = spyOn(component, "getTeam").and.returnValue(TEAM_DICT);
+            const spy_selected = spyOn(component, "selectedTeam");
+            fixture.detectChanges();
+            const select = await loader.getHarness(MatSelectHarness);
+
+            await select.open();
+            const options = await select.getOptions();
+            await options[1].click();
+
+            expect(spy_selected).toHaveBeenCalledWith(TEAMS[1]);
+        }));
+
+        it('should mat-form for select show name of team selected', fakeAsync(async () => {
+            const spy_short_select = spyOn(component, "isShortSelectedTeam").and.returnValue(true);
+            const spy_team = spyOn(component, "getTeam").and.returnValue(TEAM_DICT);
+            fixture.detectChanges();
+            const form = await loader.getHarness(MatFormFieldHarness);
+            const select = await loader.getHarness(MatSelectHarness);
+
+            await select.open();
+            const options = await select.getOptions();
+            await options[1].click();
+
+
+        }));
+
         it('should mat-input set value of input into input_text when text is written', fakeAsync(async () => {
             const input = await loader.getHarness(MatInputHarness);
 
             await input.setValue('test');
+            tick(300);
 
             expect(component.input_txt).toEqual('test');
         }));
@@ -88,3 +235,37 @@ describe('PlayerListComponent DOM', () => {
         }));
     });
 })
+
+const ROLES = [
+    'P',
+    'D',
+    'C',
+    'A'
+]
+
+const TEAMS = [
+    'Atalanta',
+    'Bologna',
+    'Cremonese'
+];
+let TEAM_DICT : {name : string, selected : boolean}[] =  [
+    {
+      name : 'Atalanta',
+      selected : false
+    },
+    {
+      name : 'Bologna',
+      selected : false
+    },
+    {
+      name : 'Cremonese',
+      selected : false
+    },
+];
+
+@Component({
+    selector: 'app-table',
+    template: ''
+  })
+  class MockTableComponent {
+  }
--- angular/myfanta-app/src/app/player-list/player-list.component.ts
+++ angular/myfanta-app/src/app/player-list/player-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
 import { debounceTime, distinctUntilChanged, Subject, switchMap } from 'rxjs';
 import { TeamDataService } from '../service/team-data.service';
 
@@ -10,6 +11,15 @@ import { TeamDataService } from '../service/team-data.service';
 export class PlayerListComponent implements OnInit {
 
   input_txt:string = '';
+  private _teams : {name : string, selected : boolean}[] = [];
+  private _roles : {name : string, selected : boolean}[] = [
+    {name : 'P', 'selected' : false},
+    {name : 'D', 'selected' : false},
+    {name : 'C', 'selected' : false},
+    {name : 'A', 'selected' : false},
+  ];
+
+  team_form = new FormControl('');
 
   private _search_players = new Subject<string>();
 
@@ -23,10 +33,86 @@ export class PlayerListComponent implements OnInit {
       // ignore new term if same as previous term
       distinctUntilChanged(),
 
-      switchMap((name:string) => this._team_data_service.searchPlayersWithName(name)),
-    );
+    ).subscribe((player_name:string) => {
+      this._team_data_service.filterPlayersByName(player_name);
+    });
+
+    this._team_data_service.getTeamName().subscribe((teams) => {
+      for(let team of teams) {
+        this._teams.push({name : team, selected : false});
+      }
+    });
   }
 
+  // GETTER
+
+  getWindowWidth() : number {
+    return window.innerWidth;
+  }
+
+  getRoles() : {name : string, selected : boolean}[] {
+    return this._roles;
+  }
+
+  getTeam() : {name : string, selected : boolean}[] {
+    return this._teams;
+  }
+
+  // METHODS
+
+  selectedRole(role : string) : void {
+    let found : any = null;
+    for(let r of this._roles.values()) {
+      if(r.name == role) {
+        r.selected = !r.selected;
+        found = r;
+        break;
+      }
+    }
+    if(found != null)
+      this.filterPlayerByRole(found);
+  }
+
+  isRoleSelected(role : string) : boolean {
+    let found = null;
+    found = this._roles.find((r) => r.name == role);
+    if(found != null)
+      return found.selected;
+    return false;
+  }
+
+  selectedTeam(team : string) : void {
+    let found : any = null;
+    for(let t of this._teams.values()) {
+      if(t.name == team) {
+        t.selected = !t.selected;
+        found = t;
+        break;
+      }
+    }
+    if(found != null)
+      this.filterPlayerByTeam(found);
+  }
+
+  isTeamSelected(team : string) : boolean {
+    let found = null;
+    found = this._teams.find((t) => t.name == team);
+    if(found != null)
+      return found.selected
+    return false;
+  }
+
+  isShortSelectedTeam() : boolean {
+    return this.getWindowWidth() <= 600 ? true : false;
+  }
+
+  filterPlayerByRole(role : {name : string, selected : boolean}) : void {
+    this._team_data_service.filterPlayerByRoles(role.name, role.selected)
+  }
+
+  filterPlayerByTeam(team : {name : string, selected : boolean}) : void {
+    this._team_data_service.filterPlayersByTeam(team.name, team.selected);
+  }
 
   filterText(event: KeyboardEvent) : boolean {
     return event.key.match(/[^a-zA-Z ,]/g) === null;
