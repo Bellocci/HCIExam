1e49de914071764caf1ac9d7adc1bfdfdf047591
Bellocci <francesco.bellocci@stud.unifi.it>
2022-09-13 16:22:21 +0200

    Updated Table component.
    # html
    - Added columns Favorite and Blacklist. It showes if player is a favorit
      player or is into blacklist. Allowed to add player to favorit player's
      list or into blacklist.
    - Added columns Expand and expandedDetail when window.width is less
      or equal than 400px. expandedDetail showes Favorit and Blacklist operation.
    
    
    # ts
    - Added animation when table showes the Expand column.
    - Added string 'Favorit' and 'Blacklist' in _columns variable.
    - expandedPlayer method set expanded_player variable to null if the argument
      is the same value of expanded_player otherwhise set _expanded_player
      value with argument.
    - isPlayerExpanded return true if expanded_player value is the same of
      argument, false otherwise.
    - isPlayerIntoBlacklist method returns the value returned by
      isPlayerIntoBlacklist method called from team service.
    - addPlayerIntoBlacklist method calls removePlayerFromFavoritList and
      addPlayerIntoBlacklist methods from team service.
    - removePlayerFromBlacklist calls removePlayerFromBlacklist method from
      team service.

--- angular/myfanta-app/src/app/table/table.component.ts
+++ angular/myfanta-app/src/app/table/table.component.ts
@@ -4,15 +4,25 @@ import { MatTableDataSource } from '@angular/material/table';
 import { MatPaginator, PageEvent } from '@angular/material/paginator';
 import { TeamDataService } from '../service/team-data.service';
 import { MatSort } from '@angular/material/sort';
+import { animate, state, style, transition, trigger } from '@angular/animations';
 
 @Component({
   selector: 'app-table',
   templateUrl: './table.component.html',
   styleUrls: ['./table.component.css'],
+  animations: [
+    trigger('detailExpand', [
+      state('collapsed', style({height: '0px', minHeight: '0'})),
+      state('expanded', style({height: '80px'})),
+      transition('expanded <=> collapsed', animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)')),
+    ]),
+  ],
+
 })
 export class TableComponent implements OnInit, AfterViewInit {
 
   private _playerSelected : any;
+  expanded_player : any | null;
 
   private _dataSource!:MatTableDataSource<any>;
   @ViewChild(MatPaginator) private _paginator!: MatPaginator;
@@ -20,11 +30,13 @@ export class TableComponent implements OnInit, AfterViewInit {
   private _pageIndex:number = 0;
   private _pageSize:number = 10;
   private _pageSizeOptions:number[] = [5, 10, 20]
-  private _columns:string[] = ['Name', 'Team', 'Role', 'Cost', 'favoritePlayer'];
+
+  private _columns:string[] = ['Name', 'Team', 'Role', 'Cost', 'Favorit', 'Blacklist'];
 
   constructor(private _team_data_service: TeamDataService) { }
 
-  ngOnInit(): void { 
+  ngOnInit(): void {
+    this.setColumns();
     this._dataSource = new MatTableDataSource<any>();
     this.subscribePlayerList();
     this.subscribePlayerSelected();
@@ -49,6 +61,10 @@ export class TableComponent implements OnInit, AfterViewInit {
 
   /* GETTER */
 
+  getInnerWidth() : number {
+    return window.innerWidth;
+  }
+
   getColumns() : string[] {
     return this._columns;
   }
@@ -69,13 +85,47 @@ export class TableComponent implements OnInit, AfterViewInit {
     return this._pageSizeOptions;
   }
 
+  /* SETTER */
+
+  setColumns() : void {
+    if(!this.isLayoutMobile()) {
+      if(this._columns.length != 6) {
+        this._columns.pop();
+        this._columns.push('Favorit');
+        this._columns.push('Blacklist');
+        this._team_data_service.setPlayerSelected(null);
+        this.expanded_player = null;
+      }
+    }
+    else {
+      if(this._columns.length != 5) {
+        this._columns.pop();
+        this._columns.pop();
+        this._columns.push('Expand')
+        this._team_data_service.setPlayerSelected(null);
+      }
+    }
+  }
+
   /* METHODS */
+
+  isLayoutMobile() : boolean {
+    return this.getInnerWidth() <= 400 ? true : false;
+  }
 
   setPlayerSelected(player:any) {
     if(this._playerSelected != player)
       this._team_data_service.setPlayerSelected(player);
     else
       this._team_data_service.setPlayerSelected(null);
+  }
+
+  expandedPlayer(player:any) : void {
+    this.expanded_player = this.expanded_player === player ? null : player
+  }
+
+  isPlayerExpanded(player:any) : boolean {
+    return this.expanded_player === player ? true : false;
   }
 
   isFavoritePlayer(player:any) : boolean {
@@ -88,6 +138,19 @@ export class TableComponent implements OnInit, AfterViewInit {
 
   removePlayerAsFavorite(player:any) : void {
     this._team_data_service.removePlayerFromFavoriteList(player);
+  }
+
+  isPlayerIntoBlacklist(player:any) : boolean {
+    return this._team_data_service.isPlayerIntoBlacklist(player);
+  }
+
+  addPlayerIntoBlacklist(player:any) : void {
+    this._team_data_service.removePlayerFromFavoriteList(player);
+    this._team_data_service.addPlayerIntoBlacklist(player);
+  }
+
+  removePlayerFromBlacklist(player:any) : void {
+    this._team_data_service.removePlayerFromBlacklist(player);
   }
 
   handlePageEvent(event: PageEvent) : void {
