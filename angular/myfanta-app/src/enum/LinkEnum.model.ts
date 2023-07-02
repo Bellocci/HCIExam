0fe05e3c8c0fac7ec845b93633427d1bbfc29264
Bellocci <francesco.bellocci@stud.unifi.it>
2023-07-02 13:32:22 +0200

    Add new link Options and updated link createdTeam

--- angular/myfanta-app/src/enum/LinkEnum.model.ts
+++ angular/myfanta-app/src/enum/LinkEnum.model.ts
@@ -5,11 +5,12 @@ Classe che racchiude le label dei link di navigazione delle pagine
 export class LinkEnum {
 
     public static readonly HOME = new LinkEnum("home", "Home");
-    public static readonly CREATE_TEAM = new LinkEnum("createTeam", "Crea Team");
+    public static readonly MYTEAM = new LinkEnum("myTeam", "My Team");
     public static readonly PLAYER_LIST = new LinkEnum("playerList", "Lista giocatori");
     public static readonly FAVORIT_LIST = new LinkEnum("favoritList", "Giocatori preferiti");
     public static readonly BLACKLIST = new LinkEnum("blacklist", "Giocatori esclusi");
     public static readonly USER_PROFILE = new LinkEnum("profile", "Mio Profilo");
+    public static readonly OPTIONS = new LinkEnum("options", "Opzioni di ricerca");
 
     private constructor(public readonly label: string, public readonly description: string) {}
 }
