7f85ced13f87a7f038971070efcfe13e1a09bc85
Bellocci <francesco.bellocci@stud.unifi.it>
2023-07-16 12:00:22 +0200

    Refactoring routing and add new path to playerProfile

--- angular/myfanta-app/src/app/app-routing.module.ts
+++ angular/myfanta-app/src/app/app-routing.module.ts
@@ -5,22 +5,59 @@ import { HomeComponent } from './home/home.component';
 import { PlayerListComponent } from './player-list/player-list.component';
 import { UserPageComponent } from './user-page/user-page.component';
 import { OptionsComponent } from './options/options.component';
+import { PlayerPageComponent } from './player-page/player-page.component';
 
 const routes: Routes = [
-  {path: "", redirectTo: '/home', pathMatch: "full"},
-  {path: 'home', component: HomeComponent},
-  {path: "home/myTeam", redirectTo: '/myTeam', pathMatch: "full"},
-  {path: 'myTeam', component: CreateTeamComponent},
-  {path: "home/playerList", redirectTo: '/playerList', pathMatch: 'full'},
-  {path: "playerList", component: PlayerListComponent},
-  {path: "home/favoritList", redirectTo: '/favoritList', pathMatch: 'full'},
-  {path: "favoritList", component: PlayerListComponent},
-  {path: "home/blackList", redirectTo: '/blackList', pathMatch: 'full'},
-  {path: "blackList", component: PlayerListComponent},
-  {path: "myProfile", component: UserPageComponent},
-  {path: "home/myProfile", redirectTo: "/myProfile", pathMatch: 'full'},
-  {path: "options", component: OptionsComponent},
-  {path: "home/options", redirectTo: "/options", pathMatch: 'full'}
+  // Serve per reindirizzare la pagina alla HomePage
+  {path: "", redirectTo: 'fantasyteam/home', pathMatch: "full"},
+
+  // HomePage
+  {
+    path: "fantasyteam",
+    children : [{ path : "home", component : HomeComponent}]
+  },
+
+  // MyTeam
+  {
+    path: "fantasyteam",
+    children : [{ path : "myTeam", component : CreateTeamComponent}]
+  },
+
+  // PlayerList
+  {
+    path: "fantasyteam",
+    children : [{ path : "playerList", component : PlayerListComponent}]
+  },
+
+  // FavoriteList
+  {
+    path: "fantasyteam",
+    children : [{ path : "favoriteList", component : PlayerListComponent}]
+  },
+
+  // Blacklist
+  {
+    path: "fantasyteam",
+    children : [{ path : "blacklist", component : PlayerListComponent}]
+  },
+
+  // MyProfile
+  {
+    path: "fantasyteam",
+    children : [{ path : "myProfile", component : UserPageComponent}]
+  },
+
+  // Options
+  {
+    path: "fantasyteam",
+    children : [{ path : "options", component : OptionsComponent}]
+  },
+
+  // PlayerPage
+  {
+    path: "fantasyteam", 
+    children : [{ path: ":league/:team/:player/:id", component: PlayerPageComponent }]
+  }
 ];
 
 @NgModule({
--- angular/myfanta-app/src/app/service/router.service.ts
+++ angular/myfanta-app/src/app/service/router.service.ts
@@ -2,19 +2,24 @@ import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
 import { InternalDataService } from './internal-data.service';
 import { LinkEnum } from 'src/enum/LinkEnum.model';
+import { Player } from 'src/decorator/player.model';
+import { Team } from 'src/decorator/team.model';
+import { League } from 'src/decorator/League.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RouterService {
 
-  private readonly HOME_PAGE_PATH:string = "";
-  private readonly MYTEAM_PAGE_PATH:string = "/myTeam";
-  private readonly PLAYER_LIST_PAGE_PATH:string = "/playerList";
-  private readonly FAVORIT_LIST_PAGE_PATH:string = "/favoritList"
-  private readonly BLACKLIST_PAGE_PATH:string = "/blackList";
-  private readonly MYPROFILE_PAGE_PATH:string = "/myProfile";
-  private readonly OPTIONS_PAGE_PATH:string = "/options";
+  private readonly PARENT_PATH:string = "/fantasyteam/";
+
+  private readonly HOME_PAGE_PATH:string = "home";
+  private readonly MYTEAM_PAGE_PATH:string = "myTeam";
+  private readonly PLAYER_LIST_PAGE_PATH:string = "playerList";
+  private readonly FAVORIT_LIST_PAGE_PATH:string = "favoriteList"
+  private readonly BLACKLIST_PAGE_PATH:string = "blackList";
+  private readonly MYPROFILE_PAGE_PATH:string = "myProfile";
+  private readonly OPTIONS_PAGE_PATH:string = "options";
 
   constructor(private router: Router, private internalDataService:InternalDataService) { }
 
@@ -23,79 +28,103 @@ export class RouterService {
   // Metodo che renderizza alla pagina di Home
   goToHomePage() : void {
     this.internalDataService.setLoadingData(true);
-    this.router.navigate([this.HOME_PAGE_PATH]);
-    this.internalDataService.setActiveLink(this.linkEnum.HOME.label);
+    this.currentPageIsHome() ? this.reloadOrNavigate(true) : this.reloadOrNavigate(false, this.PARENT_PATH + this.HOME_PAGE_PATH);
+  }
+
+  // Verifica se la pagina currente è 'home'
+  currentPageIsHome() : boolean {    
+    return this.router.url == this.PARENT_PATH + this.HOME_PAGE_PATH;
   }
 
   // Metodo che renderizza alla pagina di Create Team
   goToMyTeamPage() : void {
     this.internalDataService.setLoadingData(true);
-    this.router.navigate([this.MYTEAM_PAGE_PATH]);
-    this.internalDataService.setActiveLink(this.linkEnum.MYTEAM.label);
+    this.currentPageIsMyTeam() ? this.reloadOrNavigate(true) : this.reloadOrNavigate(false, this.PARENT_PATH + this.MYTEAM_PAGE_PATH);
   }
 
   // Verifica se la pagina corrente è 'myTeam'
   currentPageIsMyTeam() : boolean {
-    return this.router.url == this.MYTEAM_PAGE_PATH;
+    return this.router.url == this.PARENT_PATH + this.MYTEAM_PAGE_PATH;
   }
 
   // Metodo che renderizza alla pagina di Lista dei giocatori
   goToPlayerListPage() : void {
     this.internalDataService.setLoadingData(true);
-    this.router.navigate([this.PLAYER_LIST_PAGE_PATH]);
-    this.internalDataService.setActiveLink(this.linkEnum.PLAYER_LIST.label);
+    this.currentPageIsPlayerList() ? this.reloadOrNavigate(true) : this.reloadOrNavigate(false, this.PARENT_PATH + this.PLAYER_LIST_PAGE_PATH);
   }
 
   currentPageIsPlayerList() : boolean {
-    return this.router.url == this.PLAYER_LIST_PAGE_PATH;
+    return this.router.url == this.PARENT_PATH + this.PLAYER_LIST_PAGE_PATH;
   }
 
   // Metodo che renderizza alla pagina di Lista dei giocatori preferiti
   goToFavoritListPage() : void {
     this.internalDataService.setLoadingData(true);
-    this.router.navigate([this.FAVORIT_LIST_PAGE_PATH]);
-    this.internalDataService.setActiveLink(this.linkEnum.FAVORIT_LIST.label);
+    this.currentPageIsFavoritList() ? this.reloadOrNavigate(true) : this.reloadOrNavigate(false, this.PARENT_PATH + this.FAVORIT_LIST_PAGE_PATH);
   }
 
   // Verifica se la pagina corrente è 'Favorit List'
   currentPageIsFavoritList() : boolean {
-    return this.router.url == this.FAVORIT_LIST_PAGE_PATH;
+    return this.router.url == this.PARENT_PATH + this.FAVORIT_LIST_PAGE_PATH;
   }
 
   // Metodo che renderizza alla pagina di lista dei giocatori esclusi
   goToBlacklistPage() : void {
     this.internalDataService.setLoadingData(true);
-    this.router.navigate([this.BLACKLIST_PAGE_PATH]);
-    this.internalDataService.setActiveLink(this.linkEnum.BLACKLIST.label);
+    this.currentPageIsBlacklist() ? this.reloadOrNavigate(true) : this.reloadOrNavigate(false, this.PARENT_PATH + this.BLACKLIST_PAGE_PATH);
   }
 
   // Verifica se la pagina corrente è 'Blacklist'
   currentPageIsBlacklist() : boolean {
-    return this.router.url == this.BLACKLIST_PAGE_PATH;
+    return this.router.url == this.PARENT_PATH + this.BLACKLIST_PAGE_PATH;
   }
 
   // Metodo che renderizza alla pagina di profilo
   goToMyProfilePage() : void {
     this.internalDataService.setLoadingData(true);
-    this.router.navigate([this.MYPROFILE_PAGE_PATH]);
-    this.internalDataService.setActiveLink(this.linkEnum.USER_PROFILE.label);
+    this.currentPageIsMyProfile() ? this.reloadOrNavigate(true) : this.reloadOrNavigate(false, this.PARENT_PATH + this.MYPROFILE_PAGE_PATH);
   }
 
   // Verifica se la pagina corrente è myProfile
   currentPageIsMyProfile() : boolean {
-    return this.router.url == this.MYPROFILE_PAGE_PATH;
+    return this.router.url == this.PARENT_PATH + this.MYPROFILE_PAGE_PATH;
   }
 
   // Metodo che renderizza alla pagina di opzioni di filtraggio dei giocatori
   goToOptionsPage() : void {
     this.internalDataService.setLoadingData(true);
-    this.router.navigate([this.OPTIONS_PAGE_PATH]);
-    this.internalDataService.setActiveLink(this.linkEnum.OPTIONS.label);
+    this.currentPageIsOptions() ? this.reloadOrNavigate(true) : this.reloadOrNavigate(false, this.PARENT_PATH + this.OPTIONS_PAGE_PATH);
   }
 
   // Verifica se la pagina corrente è myProfile
   currentPageIsOptions() : boolean {
-    return this.router.url == this.OPTIONS_PAGE_PATH;
+    return this.router.url == this.PARENT_PATH + this.OPTIONS_PAGE_PATH;
+  }
+
+  goToPlayerPage(player:Player) : void {
+    this.internalDataService.setLoadingData(true);
+    const team:Team = player.getTeam();
+    const league:League = team.getLeague();
+    this.currentPageIsPlayerProfile(player) ? this.reloadOrNavigate(true) : 
+      this.reloadOrNavigate(false, this.PARENT_PATH + league.getName().replace(/[^a-zA-Z0-9]/g, "") + "/" + team.getName() 
+      + "/" + player.getName() + "/" + player.getId());
+  }
+
+  currentPageIsPlayerProfile(player:Player) : boolean {
+    const team:Team = player.getTeam();
+    const league:League = team.getLeague();
+    return this.router.url == this.PARENT_PATH + league.getName() + "/" + team.getName() + "/" + player.getName() + "/" + player.getId();
   }
   
+  /**
+   * Metodo che ricarica la pagina corrente o naviga all'url passato come argomento 
+   * 
+   * @param self : boolean
+   * @param urlToNavigateTo : string (Obbligatorio solo se self è false)
+   */
+  private reloadOrNavigate(self:boolean,urlToNavigateTo ?:string){
+    //skipLocationChange:true means dont update the url to / when navigating
+    const url=self ? this.router.url :urlToNavigateTo;
+    this.router.navigateByUrl('/',{skipLocationChange:true}).then(() => this.router.navigate([`/${url}`]) );
+  }
 }
