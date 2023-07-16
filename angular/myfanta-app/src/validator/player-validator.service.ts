12ea93e3c60763eef0fb20014f607dcc21d4dcae
Bellocci <francesco.bellocci@stud.unifi.it>
2023-07-16 11:06:00 +0200

    Add new service PlayerValidatorService

---
+++ angular/myfanta-app/src/validator/player-validator.service.ts
@@ -0,0 +1,57 @@
+import { Injectable } from '@angular/core';
+import { InternalDataService } from 'src/app/service/internal-data.service';
+import { TeamDataService } from 'src/app/service/team-data.service';
+import { OptionFootballSoccer } from 'src/decorator/option/optionFootballSoccer.model';
+import { Player } from 'src/decorator/player.model';
+import { Result } from 'src/decorator/result/result.interface';
+import { ResultImpl } from 'src/decorator/result/resultImpl';
+import { SportEnum } from 'src/enum/SportEnum.model';
+import { OptionFootballSoccerEnum } from 'src/enum/optionEnum/OptionFootballSoccerEnum.model';
+import { SportEnumVisitorAbstract } from 'src/visitor/sport-enum/SportEnumVisitorAbstract';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PlayerValidatorService {
+
+  constructor(private teamDataService:TeamDataService) { }
+
+  canAddPlayerToTeam(player:Player, sport:SportEnum) : Result {
+    let errorMessage:string[] = [];
+    const roleMap:Map<string,number> = this.getMaxPlayerForRole(sport);
+    let numOfPlayer:number = this.teamDataService.getUserTeamList().filter(p => p.getRole() == player.getRole()).length;
+    return numOfPlayer < roleMap.get(player.getRole())! ? new ResultImpl(true, []) :
+        new ResultImpl(false, ["Raggiunto il numero massimo di giocatori per il ruolo " + player.getRole()]);
+  }  
+
+  private getMaxPlayerForRole(sport:SportEnum) : Map<string,number> {
+  
+    return SportEnum.visitAndReturn(sport, {
+      footballSoccer() : Map<string,number> {
+        let result:Map<string,number> = new Map();
+        // Portieri
+        result.set(OptionFootballSoccerEnum.MAX_GOALKEEPER_PLAYERS.abbreviation!, 
+          OptionFootballSoccerEnum.MAX_GOALKEEPER_PLAYERS.value as number);
+        // Difensori
+        result.set(OptionFootballSoccerEnum.MAX_DEFENDER_PLAYERS.abbreviation!, 
+          OptionFootballSoccerEnum.MAX_DEFENDER_PLAYERS.value as number);
+        // Centrocampisti
+        result.set(OptionFootballSoccerEnum.MAX_MIDFIELDER_PLAYERS.abbreviation!, 
+          OptionFootballSoccerEnum.MAX_MIDFIELDER_PLAYERS.value as number);
+        // Attaccanti
+        result.set(OptionFootballSoccerEnum.MAX_STRAIKERS_PLAYERS.abbreviation!, 
+          OptionFootballSoccerEnum.MAX_STRAIKERS_PLAYERS.value as number);
+
+        return result;
+      },
+
+      volleyball() : Map<string,number> {
+        return new Map();
+      },
+
+      basketball() : Map<string,number> {
+        return new Map();
+      },
+    })
+  }
+}
