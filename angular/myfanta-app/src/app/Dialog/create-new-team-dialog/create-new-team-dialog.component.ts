0a8daea9fa1fc24adb2430818b486b77c8720666
Bellocci <francesco.bellocci@stud.unifi.it>
2024-03-29 21:18:17 +0100

    Create New Team Dialog component responsive

--- angular/myfanta-app/src/app/Dialog/create-new-team-dialog/create-new-team-dialog.component.ts
+++ angular/myfanta-app/src/app/Dialog/create-new-team-dialog/create-new-team-dialog.component.ts
@@ -1,5 +1,4 @@
 import { Component, Inject, OnDestroy, OnInit } from '@angular/core';
-import { FormControl, Validators } from '@angular/forms';
 import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { DialogService } from 'src/app/service/dialog.service';
 import { FilterDataService } from 'src/app/service/filter-data.service';
@@ -15,11 +14,12 @@ import { Subscription } from 'rxjs';
 import { BreakpointsService } from 'src/app/service/breakpoints.service';
 import { ObserverStepBuilder } from 'src/utility/observer-step-builder';
 import { UserTeamDialogData } from './user-team-dialog-data';
+import { FilterUtility } from 'src/utility/filter-utility';
 
 @Component({
   selector: 'app-create-new-team-dialog',
   templateUrl: './create-new-team-dialog.component.html',
-  styleUrls: ['./create-new-team-dialog.component.scss'],
+  styleUrls: ['./create-new-team-dialog.component.scss']
 })
 export class CreateNewTeamDialogComponent implements OnInit, OnDestroy {
 
@@ -28,11 +28,15 @@ export class CreateNewTeamDialogComponent implements OnInit, OnDestroy {
    * VARIABILI 
    * ==========
    */  
-  private showSelected:boolean = true;
-  teamNameFormControl:FormControl = new FormControl('My team', { validators : [ Validators.required] });
-  private selectedSport:SportEnum | string = "";
-  private selectedChampionship:ChampionshipEnum | string = "";
-  private selectedLeague:LeagueEntity | null = null;
+
+  private static readonly TEAM_NAME_DEFAULT: string = "Nome squadra";
+
+  private _isCreateMode!: boolean;
+  private _teamName: string;  
+  private _selectedSport!: SportEnum | undefined;  
+  private _selectedChampionship: ChampionshipEnum | undefined;
+  private _selectedLeague: LeagueEntity | undefined;
+  
   private importPlayer:boolean = false;
 
   private championshipsList:ChampionshipEnum[] = [];
@@ -57,27 +61,32 @@ export class CreateNewTeamDialogComponent implements OnInit, OnDestroy {
 
       console.log("Construct New Team dialog component");
 
+      this._teamName = CreateNewTeamDialogComponent.TEAM_NAME_DEFAULT;
       this._subscriptionToMobileBreakpointObservable = this.observeMobileBreakpoint();
-  }    
 
-  ngOnInit(): void {
-    if(this.dialogData.isCreateMode && this.dialogData.userTeam != undefined) {
-        this.teamNameFormControl.setValue(this.dialogData.userTeam.nameTeam);
-        this.selectedSport = this.dialogData.userTeam.league.sport
-        this.selectedChampionship = this.dialogData.userTeam.league.championship;
-        this.selectedLeague = this.dialogData.userTeam.league;               
-    } 
-    else if(!this.dialogData.isCreateMode) {
-      if(this.dialogData.userTeam == undefined) {
-        throw Error("Unable to edit user team " + this.dialogData.userTeam);
-      }
-
-      this.teamNameFormControl.setValue(this.dialogData.userTeam.nameTeam);
+      this.isCreateMode = this.dialogData.isCreateMode != undefined ? 
+        this.dialogData.isCreateMode :
+        true;
+  
+      if(this.dialogData.isCreateMode && this.dialogData.userTeam != undefined) {
+          this.teamName = this.dialogData.userTeam.nameTeam;
+          this.selectedSport = this.dialogData.userTeam.league.sport
+          this.selectedChampionship = this.dialogData.userTeam.league.championship;
+          this.selectedLeague = this.dialogData.userTeam.league;               
+      } 
+      else if(!this.dialogData.isCreateMode) {
+        if(this.dialogData.userTeam == undefined) {
+          throw Error("Unable to edit user team " + this.dialogData.userTeam);
+        }
+  
+        this.teamName = this.dialogData.userTeam.nameTeam;
         this.selectedSport = this.dialogData.userTeam.league.sport
         this.selectedChampionship = this.dialogData.userTeam.league.championship;
         this.selectedLeague = this.dialogData.userTeam.league;
-    }
-  }
+      }
+  }    
+
+  ngOnInit(): void { }
 
   ngOnDestroy(): void {
     console.log("Destroyer New Team Dialog");
@@ -99,7 +108,51 @@ export class CreateNewTeamDialogComponent implements OnInit, OnDestroy {
         .build())    
   }
 
-  /* GETTER */
+  /*
+   * ================
+   * GETTER & SETTER 
+   * ================
+   */
+
+  public get isCreateMode(): boolean {
+    return this._isCreateMode;
+  }
+
+  private set isCreateMode(value: boolean) {
+    this._isCreateMode = value;
+  }
+
+  public get teamName(): string {
+    return this._teamName;
+  }
+
+  public set teamName(value: string) {
+    this._teamName = value;
+  }
+
+  public get selectedSport(): SportEnum | undefined {
+    return this._selectedSport;
+  }
+  
+  public set selectedSport(value: SportEnum | undefined) {
+    this._selectedSport = value;
+  }
+
+  public get selectedChampionship(): ChampionshipEnum | undefined {
+    return this._selectedChampionship;
+  }
+
+  public set selectedChampionship(value: ChampionshipEnum | undefined) {
+    this._selectedChampionship = value;
+  }
+
+  public get selectedLeague(): LeagueEntity | undefined {
+    return this._selectedLeague;
+  }
+
+  public set selectedLeague(value: LeagueEntity | undefined) {
+    this._selectedLeague = value;
+  }
 
   public get isMobileBreakpointActive(): boolean {
     return this._isMobileBreakpointActive;
@@ -121,16 +174,16 @@ export class CreateNewTeamDialogComponent implements OnInit, OnDestroy {
     return this.leaguesList;
   }
 
-  getSportSelected() : string {
-    return this.selectedSport instanceof SportEnum ? this.selectedSport.description : this.selectedSport;
+  getSportDescription() : string {
+    return this.selectedSport != undefined ? this.selectedSport.description : '';
   }
 
-  getChampionshipSelected() : string {
-    return this.selectedChampionship instanceof ChampionshipEnum ? this.selectedChampionship.description : this.selectedChampionship;
+  getChampionshipDescription() : string {
+    return this.selectedChampionship != undefined ? this.selectedChampionship.description : '';
   }
 
-  getLeagueSelected() : string {
-    return this.selectedLeague != null ? this.selectedLeague.name : '';
+  getLeagueDescription() : string {
+    return this.selectedLeague != undefined ? this.selectedLeague.name : '';
   }
 
   getAddTooltipMessage() : string {
@@ -147,60 +200,25 @@ export class CreateNewTeamDialogComponent implements OnInit, OnDestroy {
     return "Crea nuova squadra";
   }
 
-  /* METODI LISTENER */
+  /*
+   * ===================
+   * METODI VISIBILITA'
+   * ===================
+   */
 
-  closeDialog(dialogResult?:any) : void {
-    this.dialogService.getDialogHelper().closeDialog(dialogResult);
+  private isTeamNameDefined() : boolean {
+    return !FilterUtility.isStringEmpty(this.teamName);
   }
 
-  updateSelectedSport(sport:SportEnum | string) {
-    this.selectedSport = sport;
-    sport instanceof SportEnum ? 
-      this.championshipsList = this.filterDataService.filterChampionshipsBySport(sport) :
-      this.championshipsList = [];
-  }
-
-  updateSelectedChampionship(championship:ChampionshipEnum | string) {
-    this.selectedChampionship = championship;
-    championship instanceof ChampionshipEnum ? 
-      this.leaguesList = this.filterDataService.filterLeaguesByChampionshipAndSport(this.selectedSport as SportEnum, championship) :
-      this.leaguesList = [];
-  }
-
-  updateSelectedLeague(league:LeagueEntity | null) {
-    this.selectedLeague = league;
-  }
-
-  addNewTeam() : void {
-    let newTeam:UserTeamEntity;
-    if(this.importPlayer) {
-      newTeam = this.userTeamDecoratorFactory.createNewUserTeam(this.userService.getUser(), 
-        this.teamNameFormControl.value, this.selectedLeague!, this.teamDataService.getMyTeamList(), 
-        this.teamDataService.getFavoritePlayersList(), this.teamDataService.getBlacklistPlayers());
-    } else {
-      newTeam = this.userTeamDecoratorFactory.createNewUserTeam(this.userService.getUser(), 
-        this.teamNameFormControl.value, this.selectedLeague!);
-    }
-    this.userService.addNewTeam(newTeam);
-    this.snackBarService.openInfoSnackBar("Nuova squadra creata con successo");
-    this.closeDialog(newTeam);
-  }
-
-  /* Metodi visibilità */
-
-  isTeamNameDefined() : boolean {
-    return this.teamNameFormControl.valid;
-  }
-
-  isSportSelected() : boolean {
+  private isSportSelected() : boolean {
     return this.selectedSport instanceof SportEnum;
   }
 
-  isChampionshipSelected() : boolean {
+  private isChampionshipSelected() : boolean {
     return this.selectedChampionship instanceof ChampionshipEnum;
   }
 
-  isLeagueSelected() : boolean {
+  private isLeagueSelected() : boolean {
     return this.selectedLeague != null;
   }
 
@@ -217,7 +235,55 @@ export class CreateNewTeamDialogComponent implements OnInit, OnDestroy {
       this.isChampionshipSelected() && this.isLeagueSelected();
   }
 
-  areSelectedRendered() : boolean {
-    return this.showSelected;
+  /*
+   * ================ 
+   * METODI LISTENER
+   * ================
+   */
+
+  closeDialog(dialogResult?:any) : void {
+    this.dialogService.getDialogHelper().closeDialog(dialogResult);
   }
+
+  updateChampsionshipsList() : void {
+    if(this.selectedSport != undefined) {
+      this.championshipsList = this.filterDataService.filterChampionshipsBySport(this.selectedSport);
+    }
+  }
+
+  resetChampionshipAndLeagueSelected() : void {
+    this.championshipsList = [];
+    this.leaguesList = [];
+  }
+
+  updateLeaguesList() : void {
+    if(this.selectedSport != undefined && this.selectedChampionship != undefined) {
+      this.leaguesList = this.filterDataService.filterLeaguesByChampionshipAndSport(this.selectedSport, this.selectedChampionship);
+    }
+  }
+
+  resetLeagueSelected() : void {
+    this.selectedLeague = undefined;
+  }
+
+  addNewTeam() : void {
+    let newTeam:UserTeamEntity;
+    if(this.importPlayer) {
+      newTeam = this.userTeamDecoratorFactory.createNewUserTeam(this.userService.getUser(), 
+        this.teamName, this.selectedLeague!, this.teamDataService.getMyTeamList(), 
+        this.teamDataService.getFavoritePlayersList(), this.teamDataService.getBlacklistPlayers());
+    } else {
+      newTeam = this.userTeamDecoratorFactory.createNewUserTeam(this.userService.getUser(), 
+        this.teamName, this.selectedLeague!);
+    }
+    this.userService.addNewTeam(newTeam);
+    this.snackBarService.openInfoSnackBar("Nuova squadra creata con successo");
+    this.closeDialog(newTeam);
+  }
+
+  updateTeam() : void {
+    this.snackBarService.openInfoSnackBar("Modificato nome della squadra : " + this.teamName);
+    this.dialogData.userTeam.nameTeam = this.teamName;
+    this.closeDialog();
+  }  
 }
