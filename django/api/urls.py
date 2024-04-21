f67d8928001294ef22cc4ae5522eab99af8c5371
Bellocci <francesco.bellocci@stud.unifi.it>
2024-04-21 11:08:31 +0200

    Move out myfanta-app from angular directory

--- django/api/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import re_path
-from api import views
-
-urlpatterns = [
-    re_path(r'^sports/$', views.getSportsList),
-    re_path(r'^championships/$', views.getChampionshipsList),
-]
